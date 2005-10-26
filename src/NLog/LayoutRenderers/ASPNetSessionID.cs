34b11412d7a2791051deff43a8c618aa234ffcc4
Jarek Kowalski <jaak@jkowalski.net>
2005-10-26 08:15:10 +0000

    Added ${aspnet-sessionid} layout renderer

--- src/NLog/LayoutRenderers/ASPNetSessionID.cs
+++ src/NLog/LayoutRenderers/ASPNetSessionID.cs
@@ -41,22 +41,11 @@ using System.Web;
 namespace NLog.LayoutRenderers
 {
     /// <summary>
-    /// ASP.NET Session variable.
-    /// </summary>
-    [LayoutRenderer("aspnet-session")]
-    public class ASPNETSessionValueLayoutRenderer: LayoutRenderer
+    /// ASP.NET Session ID.
+    /// </summary>                 
+    [LayoutRenderer("aspnet-sessionid")]
+    public class ASPNETSessionIDLayoutRenderer: LayoutRenderer
     {
-        private string _variable = null;
-        
-        /// <summary>
-        /// Session variable name.
-        /// </summary>
-        public string Variable
-        {
-            get { return _variable; }
-            set { _variable = value; }
-        }
-        
         /// <summary>
         /// Returns the estimated number of characters that are needed to
         /// hold the rendered value for the specified logging event.
@@ -70,22 +59,16 @@ namespace NLog.LayoutRenderers
         /// </remarks>
         protected internal override int GetEstimatedBufferSize(LogEventInfo logEvent)
         {
-            if (Variable == null)
-                return 0;
-            else
-                return 64;
+            return 64;
         }
 
         /// <summary>
-        /// Renders the specified ASP.NET Session value and appends it to the specified <see cref="StringBuilder" />.
+        /// Renders the ASP.NET Session ID appends it to the specified <see cref="StringBuilder" />.
         /// </summary>
         /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
         /// <param name="logEvent">Logging event.</param>
         protected internal override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
-            if (Variable == null)
-                return ;
-
             HttpContext context = HttpContext.Current;
             if (context == null)
                 return ;
@@ -93,7 +76,7 @@ namespace NLog.LayoutRenderers
 			if (context.Session == null)
 				return ;
 
-            builder.Append(context.Session[Variable]);
+            builder.Append(context.Session.SessionID);
         }
     }
 }
