67f57bc7e02543d44b9bc60cb6d6b2cfe8953def
Jarek Kowalski <jaak@jkowalski.net>
2006-02-14 07:14:55 +0000

    some docs

--- src/NLog/Web/NLogHttpModule.cs
+++ src/NLog/Web/NLogHttpModule.cs
@@ -43,22 +43,39 @@ using NLog.Config;
 
 namespace NLog.Web
 {
+    /// <summary>
+    /// ASP.NET HttpModule that enables NLog to hook BeginRequest and EndRequest events easily.
+    /// </summary>
     public class NLogHttpModule : IHttpModule
     {
+        /// <summary>
+        /// Event to be raised at the end of each HTTP Request.
+        /// </summary>
         public static event EventHandler EndRequest;
+
+        /// <summary>
+        /// Event to be raised at the beginning of each HTTP Request.
+        /// </summary>
         public static event EventHandler BeginRequest;
-        
+
+        /// <summary>
+        /// Initializes the HttpModule
+        /// </summary>
+        /// <param name="application">ASP.NET application</param>
         public void Init(HttpApplication application)
         {
             application.BeginRequest += new EventHandler(this.BeginRequestHandler);
             application.EndRequest += new EventHandler(this.EndRequestHandler);
         }
 
+        /// <summary>
+        /// Disposes the module.
+        /// </summary>
         public void Dispose()
         {
         }
 
-        public void BeginRequestHandler(object sender, EventArgs args)
+        private void BeginRequestHandler(object sender, EventArgs args)
         {
             if (BeginRequest != null)
             {
@@ -66,7 +83,7 @@ namespace NLog.Web
             }
         }
 
-        public void EndRequestHandler(object sender, EventArgs args)
+        private void EndRequestHandler(object sender, EventArgs args)
         {
             if (EndRequest != null)
             {
