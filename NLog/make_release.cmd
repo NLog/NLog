968fec2ede6f5d420d3e6aa37770d42aadcf2042
Jarek Kowalski <jaak@jkowalski.net>
2009-09-07 17:52:03 +0000

    preparing for 1.0 Refresh release

---
+++ NLog/make_release.cmd
@@ -0,0 +1,8 @@
+@echo off
+if (%1)==() (
+	echo Usage: %0 releaseName
+	echo.
+	echo releaseName is the name of the release without NLog prefix
+	exit /b 1
+)
+nant -D:nlog.package.name=%1 fullrelease
