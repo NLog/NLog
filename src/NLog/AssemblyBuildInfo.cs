3f67f90a709e7af052c97ad9df5b0fef0d860b0c
Jarek Kowalski <jaak@jkowalski.net>
2009-09-19 19:58:24 +0000

    fixing svn ignores

---
+++ src/NLog/AssemblyBuildInfo.cs
@@ -0,0 +1,5 @@
+// do not modify this file. It will be automatically regenerated
+// based on the version number saved in 'D:\Work\NLog2\src\NLog\..\..\NLog.version'
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.0.0.0")]
