5ffcb3c2e36ecf3e21ae7938c2a6925e37e9d444
Jarek Kowalski <jaak@jkowalski.net>
2010-02-22 07:10:09 +0000

    updated license headers

--- tools/DumpTestResultSummary/Properties/AssemblyInfo.cs
+++ tools/DumpTestResultSummary/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("DumpTestResultSummary")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Jaroslaw Kowalski 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
