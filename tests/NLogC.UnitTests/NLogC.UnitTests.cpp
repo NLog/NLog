099c2b9206581444b7fc3d9d570ef79b8eaf4ab5
Jarek Kowalski <jaak@jkowalski.net>
2010-07-08 19:35:35 -0700

    Updates to NLogC, fixed nighly build which was broken by race condition.

--- tests/NLogC.UnitTests/NLogC.UnitTests.cpp
+++ tests/NLogC.UnitTests/NLogC.UnitTests.cpp
@@ -44,91 +44,101 @@
 
 void GetOutputFileName(TCHAR *buffer, int bufferSize)
 {
-	TCHAR *p;
+    TCHAR *p;
 
-	// compute the name of the output file
-	GetModuleFileName(NULL, buffer, bufferSize);
-	p = _tcsrchr(buffer, _T('\\'));
-	_tcscpy(p, _T("\\nlogc.out"));
+    // compute the name of the output file
+    GetModuleFileName(NULL, buffer, bufferSize);
+    p = _tcsrchr(buffer, _T('\\'));
+    _tcscpy(p, _T("\\nlogc.out"));
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	// NLog_Init(_T("NLog.dll"));
-	// NLog_InitLocal(); 
+    // NLog_Init(_T("NLog.dll"));
+    // NLog_InitLocal(); 
 
-	TCHAR outputFile[MAX_PATH];
-	GetOutputFileName(outputFile, sizeof(outputFile) / sizeof(outputFile[0]));
+    TCHAR outputFile[MAX_PATH];
+    GetOutputFileName(outputFile, sizeof(outputFile) / sizeof(outputFile[0]));
 
-	DeleteFile(outputFile);
+    DeleteFile(outputFile);
 
-	// output log messages using TCHAR strings
-	NLog_Trace(_T("logger1"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
-	NLog_Debug(_T("logger2"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
-	NLog_Info(_T("logger3"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
-	NLog_Warn(_T("logger4"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
-	NLog_Error(_T("logger5"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
-	NLog_Fatal(_T("logger6"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    // output log messages using TCHAR strings
+    NLog_Trace(_T("logger1"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    NLog_Debug(_T("logger2"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    NLog_Info(_T("logger3"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    NLog_Warn(_T("logger4"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    NLog_Error(_T("logger5"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
+    NLog_Fatal(_T("logger6"), _T("message %d %d '%s'"), 10, 20, _T("foo"));
 
-	// output log messages using Unicode strings
-	NLog_TraceW(L"logger1", L"message %d %d '%s'", 10, 20, L"foo");
-	NLog_DebugW(L"logger2", L"message %d %d '%s'", 10, 20, L"foo");
-	NLog_InfoW(L"logger3", L"message %d %d '%s'", 10, 20, L"foo");
-	NLog_WarnW(L"logger4", L"message %d %d '%s'", 10, 20, L"foo");
-	NLog_ErrorW(L"logger5", L"message %d %d '%s'", 10, 20, L"foo");
-	NLog_FatalW(L"logger6", L"message %d %d '%s'", 10, 20, L"foo");
+    // output log messages using Unicode strings
+    NLog_TraceW(L"logger1", L"message %d %d '%s'", 10, 20, L"foo");
+    NLog_DebugW(L"logger2", L"message %d %d '%s'", 10, 20, L"foo");
+    NLog_InfoW(L"logger3", L"message %d %d '%s'", 10, 20, L"foo");
+    NLog_WarnW(L"logger4", L"message %d %d '%s'", 10, 20, L"foo");
+    NLog_ErrorW(L"logger5", L"message %d %d '%s'", 10, 20, L"foo");
+    NLog_FatalW(L"logger6", L"message %d %d '%s'", 10, 20, L"foo");
 
-	// output log messages using ANSI strings
-	NLog_TraceA("logger1", "message %d %d '%s'", 10, 20, "foo");
-	NLog_DebugA("logger2", "message %d %d '%s'", 10, 20, "foo");
-	NLog_InfoA("logger3", "message %d %d '%s'", 10, 20, "foo");
-	NLog_WarnA("logger4", "message %d %d '%s'", 10, 20, "foo");
-	NLog_ErrorA("logger5", "message %d %d '%s'", 10, 20, "foo");
-	NLog_FatalA("logger6", "message %d %d '%s'", 10, 20, "foo");
+    // output log messages using ANSI strings
+    NLog_TraceA("logger1", "message %d %d '%s'", 10, 20, "foo");
+    NLog_DebugA("logger2", "message %d %d '%s'", 10, 20, "foo");
+    NLog_InfoA("logger3", "message %d %d '%s'", 10, 20, "foo");
+    NLog_WarnA("logger4", "message %d %d '%s'", 10, 20, "foo");
+    NLog_ErrorA("logger5", "message %d %d '%s'", 10, 20, "foo");
+    NLog_FatalA("logger6", "message %d %d '%s'", 10, 20, "foo");
 
-	int errorCount = 0;
-	FILE *in = _tfopen(outputFile, _T("rt"));
-	if (in)
-	{
-		char line[512];
-		int l = 1;
-		
-		while (fgets(line, 512, in))
-		{
-			char *tok = strtok(line, "|\n");
-			char *tok2 = strtok(0, "|\n");
-			char *tok3 = strtok(0, "|\n");
+    int errorCount = 0;
+    FILE *in = _tfopen(outputFile, _T("rt"));
+    if (in)
+    {
+        char line[512];
+        int l = 1;
+        
+        while (fgets(line, 512, in))
+        {
+            char *tok = strtok(line, "|\n");
+            char *tok2 = strtok(0, "|\n");
+            char *tok3 = strtok(0, "|\n");
 
-			if (0 != strcmp(tok2, "message 10 20 'foo'"))
-			{
-				printf("ERROR: Invalid message in line %d.\n", l);
-				errorCount++;
-			}
+            if (0 != strcmp(tok2, "message 10 20 'foo'"))
+            {
+                printf("ERROR: Invalid message in line %d.\n", l);
+                errorCount++;
+            }
 
-			int line;
-			sscanf(tok3, "%d", &line);
-			if (line != l)
-			{
-				printf("ERROR: Invalid counter in line %d.\n", l);
-				errorCount++;
-			}
+            int lineNumber;
+            if (sscanf(tok3, "%d", &lineNumber) != 1)
+            {
+                printf("ERROR: Invalid counter number format in line %d.\n", l);
+                errorCount++;
+            }
 
-			l++;
-		}
+            if (lineNumber != l)
+            {
+                printf("ERROR: Invalid counter in line %d.\n", l);
+                errorCount++;
+            }
 
-		if (l != 19)
-		{
-			printf("ERROR: Invalid number of lines %d\n", l);
-			errorCount++;
-		}
+            l++;
+        }
 
-		fclose(in);
-	}
+        if (l != 19)
+        {
+            printf("ERROR: Invalid number of lines %d\n", l);
+            errorCount++;
+        }
 
-	if (errorCount == 0)
-	{
-		printf("PASSED\n");
-	}
+        fclose(in);
+    }
+    else
+    {
+        _tprintf(_T("ERROR: Log file not found: %s!\n"), outputFile);
+        errorCount++;
+    }
 
-	return errorCount;
+    if (errorCount == 0)
+    {
+        printf("PASSED\n");
+    }
+
+    return errorCount;
 }
