86924285edab928141dce719c9fe1520bddce8bb
Jarek Kowalski <jaak@jkowalski.net>
2011-04-17 21:16:48 -0700

    Added 3 new layout renderer wrappers for conditional output.
    ${when} - only outputs the nested layout when the specified condition is met.
    ${onexception} - only outputs the nested layout when the log event is associated with an exception
    ${whenempty} - outputs alternative layout when the nested layout produces empty result.
    
    Examples:
    
    ${message:whenempty=<no message>}
    ${message}${onexception:EXCEPTION OCCURED:${exception:format=tostring}}
    ${message:when=logger=='MyLogger1'}

---
+++ tests/NLog.UnitTests/LayoutRenderers/Wrappers/OnExceptionTests.cs
@@ -0,0 +1,67 @@
+// 
+// Copyright (c) 2004-2010 Jaroslaw Kowalski <jaak@jkowalski.net>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+namespace NLog.UnitTests.LayoutRenderers.Wrappers
+{
+    using System;
+    using NLog;
+    using NUnit.Framework;
+
+#if !NUNIT
+    using SetUp = Microsoft.VisualStudio.TestTools.UnitTesting.TestInitializeAttribute;
+    using TestFixture = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+    using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+    using TearDown =  Microsoft.VisualStudio.TestTools.UnitTesting.TestCleanupAttribute;
+#endif
+    using NLog.Internal;
+    using NLog.Layouts;
+
+    [TestFixture]
+    public class OnExceptionTests : NLogTestBase
+    {
+        [Test]
+        public void OnExceptionTest1()
+        {
+            SimpleLayout l = @"${message}${onexception:EXCEPTION\:${exception:format=message}}${logger}";
+
+            // no exception - ${onexception} is ignored completely
+            var le = LogEventInfo.Create(LogLevel.Info, "logger", "message");
+            Assert.AreEqual("messagelogger", l.Render(le));
+
+            // have exception
+            var le2 = LogEventInfo.Create(LogLevel.Info, "logger", "message");
+            le2.Exception = new InvalidOperationException("ExceptionMessage");
+            Assert.AreEqual("messageEXCEPTION:ExceptionMessagelogger", l.Render(le2));
+        }
+    }
+}
