a9a35e268e3817da906e0cda7166474bd8818794
Jarek Kowalski <jaak@jkowalski.net>
2011-07-15 07:01:34 -0700

    Added ${json-encode} wrapper.

---
+++ src/NLog/LayoutRenderers/Wrappers/JsonEncodeLayoutRendererWrapper.cs
@@ -0,0 +1,138 @@
+// 
+// Copyright (c) 2004-2011 Jaroslaw Kowalski <jaak@jkowalski.net>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+namespace NLog.LayoutRenderers.Wrappers
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml;
+    using NLog.Config;
+
+    /// <summary>
+    /// Escapes output of another layout using JSON rules.
+    /// </summary>
+    [LayoutRenderer("json-encode")]
+    [AmbientProperty("JsonEncode")]
+    [ThreadAgnostic]
+    public sealed class JsonEncodeLayoutRendererWrapper : WrapperLayoutRendererBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonEncodeLayoutRendererWrapper" /> class.
+        /// </summary>
+        public JsonEncodeLayoutRendererWrapper()
+        {
+            this.JsonEncode = true;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to apply JSON encoding.
+        /// </summary>
+        /// <docgen category="Transformation Options" order="10"/>
+        [DefaultValue(true)]
+        public bool JsonEncode { get; set; }
+
+        /// <summary>
+        /// Post-processes the rendered message. 
+        /// </summary>
+        /// <param name="text">The text to be post-processed.</param>
+        /// <returns>JSON-encoded string.</returns>
+        protected override string Transform(string text)
+        {
+            return this.JsonEncode ? DoJsonEscape(text) : text;
+        }
+
+        private static string DoJsonEscape(string text)
+        {
+            var sb = new StringBuilder(text.Length);
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                switch (text[i])
+                {
+                    case '"':
+                        sb.Append("\\\"");
+                        break;
+
+                    case '\\':
+                        sb.Append("\\\\");
+                        break;
+
+                    case '/':
+                        sb.Append("\\/");
+                        break;
+
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+
+                    default:
+                        if (NeedsEscaping(text[i]))
+                        {
+                            sb.Append("\\u");
+                            sb.Append(Convert.ToString((int)text[i], 16).PadLeft(4, '0'));
+                        }
+                        else
+                        {
+                            sb.Append(text[i]);
+                        }
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static bool NeedsEscaping(char ch)
+        {
+            return (ch < 32 || ch > 127);
+        }
+    }
+}
---
+++ tests/NLog.UnitTests/LayoutRenderers/Wrappers/JsonEncodeTests.cs
@@ -0,0 +1,60 @@
+// 
+// Copyright (c) 2004-2011 Jaroslaw Kowalski <jaak@jkowalski.net>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+namespace NLog.UnitTests.LayoutRenderers.Wrappers
+{
+    using NLog;
+    using NUnit.Framework;
+
+#if !NUNIT
+    using SetUp = Microsoft.VisualStudio.TestTools.UnitTesting.TestInitializeAttribute;
+    using TestFixture = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+    using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+    using TearDown =  Microsoft.VisualStudio.TestTools.UnitTesting.TestCleanupAttribute;
+#endif
+    using NLog.Layouts;
+
+    [TestFixture]
+    public class JsonEncodeTests : NLogTestBase
+    {
+        [Test]
+        public void JsonEncodeTest1()
+        {
+            MappedDiagnosticsContext.Clear();
+            MappedDiagnosticsContext.Set("foo", " abc\"\n\b\r\f\t/\u1234\u5432\\xyz ");
+            SimpleLayout l = "${json-encode:${mdc:foo}}";
+
+            Assert.AreEqual(@" abc\""\n\b\r\f\t\/\u1234\u5432\\xyz ", l.Render(LogEventInfo.CreateNullEvent()));
+        }
+    }
+}
