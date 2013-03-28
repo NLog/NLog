0056f91f5fe0e707d6f385576b403677d347af0e
Xharze <kimworking@gmail.com>
2013-03-29 00:25:52 +0100

    Cleanup FallbackGroup test fixture

--- tests/NLog.UnitTests/Targets/Wrappers/FallbackGroupTargetTests.cs
+++ tests/NLog.UnitTests/Targets/Wrappers/FallbackGroupTargetTests.cs
@@ -53,224 +53,97 @@ namespace NLog.UnitTests.Targets.Wrappers
     public class FallbackGroupTargetTests : NLogTestBase
 	{
         [Test]
-        public void FallbackGroupTargetSyncTest1()
+        public void FirstTargetWorks_Write_AllEventsAreWrittenToFirstTarget()
         {
             var myTarget1 = new MyTarget();
             var myTarget2 = new MyTarget();
             var myTarget3 = new MyTarget();
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(false, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
-
-            // no exceptions
-            for (int i = 0; i < 10; ++i)
-            {
-                wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
-            }
-
-            Assert.AreEqual(10, exceptions.Count);
-            foreach (var e in exceptions)
-            {
-                Assert.IsNull(e);
-            }
+            WriteAndAssertNoExceptions(wrapper);
 
             Assert.AreEqual(10, myTarget1.WriteCount);
             Assert.AreEqual(0, myTarget2.WriteCount);
             Assert.AreEqual(0, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
         }
 
         [Test]
-        public void FallbackGroupTargetSyncTest2()
+        public void FirstTargetFails_Write_SecondTargetWritesAllEvents()
         {
-            // fail once
-            var myTarget1 = new MyTarget() { FailCounter = 1 };
+            var myTarget1 = new MyTarget { FailCounter = 1 };
             var myTarget2 = new MyTarget();
             var myTarget3 = new MyTarget();
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(false, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
-
-            // no exceptions
-            for (int i = 0; i < 10; ++i)
-            {
-                wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
-            }
-
-            Assert.AreEqual(10, exceptions.Count);
-            foreach (var e in exceptions)
-            {
-                Assert.IsNull(e);
-            }
+            WriteAndAssertNoExceptions(wrapper);
 
             Assert.AreEqual(1, myTarget1.WriteCount);
             Assert.AreEqual(10, myTarget2.WriteCount);
             Assert.AreEqual(0, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
         }
 
         [Test]
-        public void FallbackGroupTargetSyncTest3()
+        public void FirstTwoTargetsFails_Write_ThirdTargetWritesAllEvents()
         {
-            // fail once
-            var myTarget1 = new MyTarget() { FailCounter = 1 };
-            var myTarget2 = new MyTarget() { FailCounter = 1 };
+            var myTarget1 = new MyTarget { FailCounter = 1 };
+            var myTarget2 = new MyTarget { FailCounter = 1 };
             var myTarget3 = new MyTarget();
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(false, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
-
-            // no exceptions
-            for (int i = 0; i < 10; ++i)
-            {
-                wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
-            }
-
-            Assert.AreEqual(10, exceptions.Count);
-            foreach (var e in exceptions)
-            {
-                Assert.IsNull(e);
-            }
+            WriteAndAssertNoExceptions(wrapper);
 
             Assert.AreEqual(1, myTarget1.WriteCount);
             Assert.AreEqual(1, myTarget2.WriteCount);
             Assert.AreEqual(10, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
         }
 
         [Test]
-        public void FallbackGroupTargetSyncTest4()
+        public void ReturnToFirstOnSuccessAndSecondTargetSucceeds_Write_ReturnToFirstTargetOnSuccess()
         {
-            // fail once
-            var myTarget1 = new MyTarget() { FailCounter = 1 };
+            var myTarget1 = new MyTarget { FailCounter = 1 };
             var myTarget2 = new MyTarget();
             var myTarget3 = new MyTarget();
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-                ReturnToFirstOnSuccess = true,
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(true, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
-
-            // no exceptions
-            for (int i = 0; i < 10; ++i)
-            {
-                wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
-            }
-
-            // sequence is like this:
-            // t1(fail), t2(success), t1(success), ... t1(success)
-            Assert.AreEqual(10, exceptions.Count);
-            foreach (var e in exceptions)
-            {
-                Assert.IsNull(e);
-            }
+            WriteAndAssertNoExceptions(wrapper);
 
             Assert.AreEqual(10, myTarget1.WriteCount);
             Assert.AreEqual(1, myTarget2.WriteCount);
             Assert.AreEqual(0, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
         }
 
         [Test]
         public void FallbackGroupTargetSyncTest5()
         {
             // fail once
-            var myTarget1 = new MyTarget() { FailCounter = 3 };
-            var myTarget2 = new MyTarget() { FailCounter = 3 };
-            var myTarget3 = new MyTarget() { FailCounter = 3 };
+            var myTarget1 = new MyTarget { FailCounter = 3 };
+            var myTarget2 = new MyTarget { FailCounter = 3 };
+            var myTarget3 = new MyTarget { FailCounter = 3 };
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-                ReturnToFirstOnSuccess = true,
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(true, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
+            var exceptions = new List<Exception>();
 
             // no exceptions
-            for (int i = 0; i < 10; ++i)
+            for (var i = 0; i < 10; ++i)
             {
                 wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
             }
 
             Assert.AreEqual(10, exceptions.Count);
-            for (int i = 0; i < 10; ++i)
+            for (var i = 0; i < 10; ++i)
             {
                 if (i < 3)
                 {
@@ -286,46 +159,29 @@ namespace NLog.UnitTests.Targets.Wrappers
             Assert.AreEqual(3, myTarget2.WriteCount);
             Assert.AreEqual(3, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
         }
 
         [Test]
         public void FallbackGroupTargetSyncTest6()
         {
             // fail once
-            var myTarget1 = new MyTarget() { FailCounter = 10 };
-            var myTarget2 = new MyTarget() { FailCounter = 3 };
-            var myTarget3 = new MyTarget() { FailCounter = 3 };
+            var myTarget1 = new MyTarget { FailCounter = 10 };
+            var myTarget2 = new MyTarget { FailCounter = 3 };
+            var myTarget3 = new MyTarget { FailCounter = 3 };
 
-            var wrapper = new FallbackGroupTarget()
-            {
-                Targets = { myTarget1, myTarget2, myTarget3 },
-                ReturnToFirstOnSuccess = true,
-            };
+            var wrapper = CreateAndInitializeFallbackGroupTarget(true, myTarget1, myTarget2, myTarget3);
 
-            myTarget1.Initialize(null);
-            myTarget2.Initialize(null);
-            myTarget3.Initialize(null);
-            wrapper.Initialize(null);
-
-            List<Exception> exceptions = new List<Exception>();
+            var exceptions = new List<Exception>();
 
             // no exceptions
-            for (int i = 0; i < 10; ++i)
+            for (var i = 0; i < 10; ++i)
             {
                 wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
             }
 
             Assert.AreEqual(10, exceptions.Count);
-            for (int i = 0; i < 10; ++i)
+            for (var i = 0; i < 10; ++i)
             {
                 if (i < 3)
                 {
@@ -344,62 +200,61 @@ namespace NLog.UnitTests.Targets.Wrappers
             Assert.AreEqual(10, myTarget2.WriteCount);
             Assert.AreEqual(3, myTarget3.WriteCount);
 
-            Exception flushException = null;
-            var flushHit = new ManualResetEvent(false);
-            wrapper.Flush(ex => { flushException = ex; flushHit.Set(); });
-
-            flushHit.WaitOne();
-            if (flushException != null)
-            {
-                Assert.Fail(flushException.ToString());
-            }
+            AssertNoFlushException(wrapper);
 
             Assert.AreEqual(1, myTarget1.FlushCount);
             Assert.AreEqual(1, myTarget2.FlushCount);
             Assert.AreEqual(1, myTarget3.FlushCount);
         }
 
-        public class MyAsyncTarget : Target
+        private static FallbackGroupTarget CreateAndInitializeFallbackGroupTarget(bool returnToFirstOnSuccess, params Target[] targets)
         {
-            public int FlushCount { get; private set; }
-            public int WriteCount { get; private set; }
+            var wrapper = new FallbackGroupTarget(targets)
+                              {
+                                  ReturnToFirstOnSuccess = returnToFirstOnSuccess,
+                              };
 
-            protected override void Write(LogEventInfo logEvent)
+            foreach (var target in targets)
             {
-                throw new NotSupportedException();
+                target.Initialize(null);
             }
 
-            protected override void Write(AsyncLogEventInfo logEvent)
-            {
-                Assert.IsTrue(this.FlushCount <= this.WriteCount);
-                this.WriteCount++;
-                ThreadPool.QueueUserWorkItem(
-                    s =>
-                        {
-                            if (this.ThrowExceptions)
-                            {
-                                logEvent.Continuation(new InvalidOperationException("Some problem!"));
-                                logEvent.Continuation(new InvalidOperationException("Some problem!"));
-                            }
-                            else
-                            {
-                                logEvent.Continuation(null);
-                                logEvent.Continuation(null);
-                            }
-                        });
-            }
+            wrapper.Initialize(null);
 
-            protected override void FlushAsync(AsyncContinuation asyncContinuation)
-            {
-                this.FlushCount++;
-                ThreadPool.QueueUserWorkItem(
-                    s => asyncContinuation(null));
-            }
-
-            public bool ThrowExceptions { get; set; }
+            return wrapper;
         }
 
-        class MyTarget : Target
+        private static void WriteAndAssertNoExceptions(FallbackGroupTarget wrapper)
+        {
+            var exceptions = new List<Exception>();
+            for (var i = 0; i < 10; ++i)
+            {
+                wrapper.WriteAsyncLogEvent(LogEventInfo.CreateNullEvent().WithContinuation(exceptions.Add));
+            }
+
+            Assert.AreEqual(10, exceptions.Count);
+            foreach (var e in exceptions)
+            {
+                Assert.IsNull(e);
+            }
+        }
+
+        private static void AssertNoFlushException(FallbackGroupTarget wrapper)
+        {
+            Exception flushException = null;
+            var flushHit = new ManualResetEvent(false);
+            wrapper.Flush(ex =>
+                              {
+                                  flushException = ex;
+                                  flushHit.Set();
+                              });
+
+            flushHit.WaitOne();
+            if (flushException != null)
+                Assert.Fail(flushException.ToString());
+        }
+
+        private class MyTarget : Target
         {
             public int FlushCount { get; set; }
             public int WriteCount { get; set; }
