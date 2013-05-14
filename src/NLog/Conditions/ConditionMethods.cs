1277ccede05bca893265e4eedfed3d6dc1c4c555
Tom Glastonbury <tomg@zanyants.com>
2013-05-14 16:45:23 +0100

    Implement optional parameters that work for .NET 2.0 and above, and Silverlight.

--- src/NLog/Conditions/ConditionMethods.cs
+++ src/NLog/Conditions/ConditionMethods.cs
@@ -35,6 +35,8 @@
 namespace NLog.Conditions
 {
     using System;
+    using System.Runtime.InteropServices;
+    using System.Reflection;
 
     /// <summary>
     /// A bunch of utility methods (mostly predicates) which can be used in
@@ -63,10 +65,22 @@ namespace NLog.Conditions
         /// <param name="ignoreCase">Optional. If <c>true</c>, case is ignored; if <c>false</c> (default), case is significant.</param>
         /// <returns><b>true</b> when two strings are equal, <b>false</b> otherwise.</returns>
         [ConditionMethod( "strequals" )]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not intended or expected to be called from an incompatible language." )]
-        public static bool Equals2(string firstValue, string secondValue, bool ignoreCase = false)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not called directly, only ever Invoked." )]
+        public static bool Equals2( string firstValue, string secondValue, [Optional
+#if !SILVERLIGHT
+            , DefaultParameterValue( false )] bool ignoreCase )
+#else
+            ] object ignoreCase )
+#endif
         {
-            return firstValue.Equals( secondValue, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
+#if SILVERLIGHT
+            bool ic = false;
+            if ( ignoreCase != null && ignoreCase is bool )
+                ic = ( bool ) ignoreCase;
+#else
+            bool ic = ignoreCase;
+#endif
+            return firstValue.Equals( secondValue, ic ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
         }
 
         /// <summary>
@@ -77,10 +91,22 @@ namespace NLog.Conditions
         /// <param name="ignoreCase">Optional. If <c>true</c> (default), case is ignored; if <c>false</c>, case is significant.</param>
         /// <returns><b>true</b> when the second string is a substring of the first string, <b>false</b> otherwise.</returns>
         [ConditionMethod("contains")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not intended or expected to be called from an incompatible language." )]
-        public static bool Contains( string haystack, string needle, bool ignoreCase = true )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not called directly, only ever Invoked." )]
+        public static bool Contains( string haystack, string needle, [Optional
+#if !SILVERLIGHT
+            , DefaultParameterValue( true )] bool ignoreCase )
+#else
+            ] object ignoreCase )
+#endif
         {
-            return haystack.IndexOf( needle, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal ) >= 0;
+#if SILVERLIGHT
+            bool ic = true;
+            if ( ignoreCase != null && ignoreCase is bool )
+                ic = ( bool ) ignoreCase;
+#else
+            bool ic = ignoreCase;
+#endif
+            return haystack.IndexOf( needle, ic ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal ) >= 0;
         }
 
         /// <summary>
@@ -91,10 +117,22 @@ namespace NLog.Conditions
         /// <param name="ignoreCase">Optional. If <c>true</c> (default), case is ignored; if <c>false</c>, case is significant.</param>
         /// <returns><b>true</b> when the second string is a prefix of the first string, <b>false</b> otherwise.</returns>
         [ConditionMethod("starts-with")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not intended or expected to be called from an incompatible language." )]
-        public static bool StartsWith( string haystack, string needle, bool ignoreCase = true )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not called directly, only ever Invoked." )]
+        public static bool StartsWith( string haystack, string needle, [Optional
+#if !SILVERLIGHT
+            , DefaultParameterValue( true )] bool ignoreCase )
+#else
+            ] object ignoreCase )
+#endif
         {
-            return haystack.StartsWith( needle, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
+#if SILVERLIGHT
+            bool ic = true;
+            if ( ignoreCase != null && ignoreCase is bool )
+                ic = ( bool ) ignoreCase;
+#else
+            bool ic = ignoreCase;
+#endif
+            return haystack.StartsWith( needle, ic ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
         }
 
         /// <summary>
@@ -105,10 +143,22 @@ namespace NLog.Conditions
         /// <param name="ignoreCase">Optional. If <c>true</c> (default), case is ignored; if <c>false</c>, case is significant.</param>
         /// <returns><b>true</b> when the second string is a prefix of the first string, <b>false</b> otherwise.</returns>
         [ConditionMethod("ends-with")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not intended or expected to be called from an incompatible language." )]
-        public static bool EndsWith( string haystack, string needle, bool ignoreCase = true )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Not called directly, only ever Invoked." )]
+        public static bool EndsWith( string haystack, string needle, [Optional
+#if !SILVERLIGHT
+            , DefaultParameterValue( true )] bool ignoreCase )
+#else
+            ] object ignoreCase )
+#endif
         {
-            return haystack.EndsWith( needle, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
+#if SILVERLIGHT
+            bool ic = true;
+            if ( ignoreCase != null && ignoreCase is bool )
+                ic = ( bool ) ignoreCase;
+#else
+            bool ic = ignoreCase;
+#endif
+            return haystack.EndsWith( needle, ic ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal );
         }
 
         /// <summary>
