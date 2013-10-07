dbfce9e4a9dea2dbf7c77af846d1efcd114ca3f9
Xharze <kimworking@gmail.com>
2013-10-08 00:31:29 +0200

    Bump NLog.Extended to version 2.1.0

--- src/NLog.Extended/AssemblyBuildInfo.cs
+++ src/NLog.Extended/AssemblyBuildInfo.cs
@@ -2,4 +2,4 @@
 // based on the version number saved in 'D:\Work\NLog2\src\NLog\..\..\NLog.version'
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyVersion("2.1.0")]
