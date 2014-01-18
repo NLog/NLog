2434895d8691ecd13ebfc6c23a2eba7ebbd782c1
Xharze <kimworking@gmail.com>
2014-01-19 00:54:34 +0100

    Remove AssemblyBuildInfo file from projects
    Instead use the information provide during build

--- src/NLog.Extended/AssemblyBuildInfo.cs
+++
@@ -1,5 +0,0 @@
-// do not modify this file. It will be automatically regenerated
-// based on the version number saved in 'D:\Work\NLog2\src\NLog\..\..\NLog.version'
-using System.Reflection;
-
-[assembly: AssemblyVersion("2.1.0")]
