b196b9b30cbac0873377061a0966a1bb590d8b6e
Robert Vazan <robert.vazan@example.com>
2014-04-10 17:57:32 +0200

    Implemented Logger.Swallow and Logger.SwallowAsync methods

--- tests/NLog.UnitTests/NLogTestBase.cs
+++ tests/NLog.UnitTests/NLogTestBase.cs
@@ -75,6 +75,16 @@ namespace NLog.UnitTests
             Assert.AreEqual(msg, debugTarget.LastMessage, "Unexpected last message value on '" + targetName + "'");
         }
 
+        public void AssertDebugLastMessageContains(string targetName, string msg)
+        {
+            NLog.Targets.DebugTarget debugTarget = (NLog.Targets.DebugTarget)LogManager.Configuration.FindTargetByName(targetName);
+
+            // Console.WriteLine("lastmsg: {0}", debugTarget.LastMessage);
+
+            Assert.IsNotNull(debugTarget, "Debug target '" + targetName + "' not found");
+            Assert.IsTrue(debugTarget.LastMessage.Contains(msg), "Unexpected last message value on '" + targetName + "'");
+        }
+
         public string GetDebugLastMessage(string targetName)
         {
             var debugTarget = (NLog.Targets.DebugTarget)LogManager.Configuration.FindTargetByName(targetName);
