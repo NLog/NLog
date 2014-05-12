09babfc4f7ea93107d325c32c1306d2d25cc6154
Christiaan van Bergen <christiaan@nonerds.nl>
2014-05-12 13:17:39 +0200

    Fix: #333 Delete archived files in correct order
    Fix for deleting archived files based on date in correct order (oldest
    should be deleted, not random).

--- tests/NLog.UnitTests/Targets/FileTargetTests.cs
+++ tests/NLog.UnitTests/Targets/FileTargetTests.cs
@@ -38,6 +38,7 @@ namespace NLog.UnitTests.Targets
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using NLog.Config;
     using NLog.Layouts;
@@ -291,6 +292,140 @@ namespace NLog.UnitTests.Targets
 
                 Assert.True(!File.Exists(Path.Combine(tempPath, "archive/0000.txt")));
                 Assert.True(!File.Exists(Path.Combine(tempPath, "archive/0004.txt")));
+            }
+            finally
+            {
+                LogManager.Configuration = null;
+                if (File.Exists(tempFile))
+                    File.Delete(tempFile);
+                if (Directory.Exists(tempPath))
+                    Directory.Delete(tempPath, true);
+            }
+        }
+
+        [Fact]
+        public void DeleteArchiveFilesByDate()
+        {
+            var tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var tempFile = Path.Combine(tempPath, "file.txt");
+            try
+            {
+                var ft = new FileTarget
+                {
+                    FileName = tempFile,
+                    ArchiveFileName = Path.Combine(tempPath, "archive/{#}.txt"),
+                    ArchiveAboveSize = 50,
+                    LineEnding = LineEndingMode.LF,
+                    ArchiveNumbering = ArchiveNumberingMode.Date,
+                    ArchiveDateFormat = "yyyyMMddHHmmssfff", //make sure the milliseconds are set in the filename
+                    Layout = "${message}",
+                    MaxArchiveFiles = 3
+                };
+
+                SimpleConfigurator.ConfigureForTargetLogging(ft, LogLevel.Debug);
+                //writing 19 times 10 bytes (9 char + linefeed) will result in 3 archive files and 1 current file
+                for (var i = 0; i < 19; ++i)
+                {
+                    logger.Debug("123456789");
+                    //build in a small sleep to make sure the current time is reflected in the filename
+                    //do this every 5 entries
+                    if (i%5==0)
+                        Thread.Sleep(50);
+                }
+                //Setting the Configuration to [null] will result in a 'Dump' of the current log entries
+                LogManager.Configuration = null;
+
+                var archivePath = Path.Combine(tempPath, "archive");
+                var files = Directory.GetFiles(archivePath).OrderBy(s => s);
+                //the amount of archived files may not exceed the set 'MaxArchiveFiles'
+                Assert.Equal(ft.MaxArchiveFiles, files.Count());
+                
+
+                SimpleConfigurator.ConfigureForTargetLogging(ft, LogLevel.Debug);
+                //writing just one line of 11 bytes will trigger the cleanup of old archived files
+                //as stated by the MaxArchiveFiles property, but will only delete the oldest file
+                logger.Debug("1234567890");
+                LogManager.Configuration = null;
+
+                var files2 = Directory.GetFiles(archivePath).OrderBy(s=>s);
+                Assert.Equal(ft.MaxArchiveFiles, files2.Count());
+                
+                //the oldest file should be deleted
+                Assert.DoesNotContain(files.ElementAt(0), files2);
+                //two files should still be there
+                Assert.Equal(files.ElementAt(1), files2.ElementAt(0));
+                Assert.Equal(files.ElementAt(2), files2.ElementAt(1));
+                //one new archive file shoud be created
+                Assert.DoesNotContain(files2.ElementAt(2), files);
+            }
+            finally
+            {
+                LogManager.Configuration = null;
+                if (File.Exists(tempFile))
+                    File.Delete(tempFile);
+                if (Directory.Exists(tempPath))
+                    Directory.Delete(tempPath, true);
+            }
+        }
+
+        [Fact]
+        public void DeleteArchiveFilesByDate_AlteredMaxArchive()
+        {
+            var tempPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var tempFile = Path.Combine(tempPath, "file.txt");
+            try
+            {
+                var ft = new FileTarget
+                {
+                    FileName = tempFile,
+                    ArchiveFileName = Path.Combine(tempPath, "archive/{#}.txt"),
+                    ArchiveAboveSize = 50,
+                    LineEnding = LineEndingMode.LF,
+                    ArchiveNumbering = ArchiveNumberingMode.Date,
+                    ArchiveDateFormat = "yyyyMMddHHmmssfff", //make sure the milliseconds are set in the filename
+                    Layout = "${message}",
+                    MaxArchiveFiles = 5
+                };
+
+                SimpleConfigurator.ConfigureForTargetLogging(ft, LogLevel.Debug);
+                //writing 29 times 10 bytes (9 char + linefeed) will result in 3 archive files and 1 current file
+                for (var i = 0; i < 29; ++i)
+                {
+                    logger.Debug("123456789");
+                    //build in a small sleep to make sure the current time is reflected in the filename
+                    //do this every 5 entries
+                    if (i % 5 == 0)
+                        Thread.Sleep(50);
+                }
+                //Setting the Configuration to [null] will result in a 'Dump' of the current log entries
+                LogManager.Configuration = null;
+
+                var archivePath = Path.Combine(tempPath, "archive");
+                var files = Directory.GetFiles(archivePath).OrderBy(s => s);
+                //the amount of archived files may not exceed the set 'MaxArchiveFiles'
+                Assert.Equal(ft.MaxArchiveFiles, files.Count());
+
+
+                //alter the MaxArchivedFiles
+                ft.MaxArchiveFiles = 2;
+                SimpleConfigurator.ConfigureForTargetLogging(ft, LogLevel.Debug);
+                //writing just one line of 11 bytes will trigger the cleanup of old archived files
+                //as stated by the MaxArchiveFiles property, but will only delete the oldest files
+                logger.Debug("1234567890");
+                LogManager.Configuration = null;
+
+                var files2 = Directory.GetFiles(archivePath).OrderBy(s => s);
+                Assert.Equal(ft.MaxArchiveFiles, files2.Count());
+
+                //the oldest files should be deleted
+                Assert.DoesNotContain(files.ElementAt(0), files2);                
+                Assert.DoesNotContain(files.ElementAt(1), files2);
+                Assert.DoesNotContain(files.ElementAt(2), files2);
+                Assert.DoesNotContain(files.ElementAt(3), files2);
+                //one files should still be there
+                Assert.Equal(files.ElementAt(4), files2.ElementAt(0));
+                //one new archive file shoud be created
+                Assert.DoesNotContain(files2.ElementAt(1), files);
             }
             finally
             {
