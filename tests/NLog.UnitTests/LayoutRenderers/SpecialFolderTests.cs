b84de89a6cc2a8cea69a32a0f0d861c0a932e91f
arjoe <arjoe>
2014-05-12 21:38:41 -0500

    Add unit tests for SpecialFolderLayoutRenderer

---
+++ tests/NLog.UnitTests/LayoutRenderers/SpecialFolderTests.cs
@@ -0,0 +1,78 @@
+﻿// 
+// Copyright (c) 2004-2011 Jaroslaw Kowalski <jaak@jkowalski.net>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+#if !SILVERLIGHT
+namespace NLog.UnitTests.LayoutRenderers
+{
+    using System;
+    using System.IO;
+    using Xunit;
+
+    /// <summary>
+    ///     Provides Unit testing for <see cref="NLog.LayoutRenderers.SpecialFolderLayoutRenderer"/>
+    /// </summary>
+    public class SpecialFolderTests : NLogTestBase
+    {
+        private string sysDir = Environment.GetFolderPath(Environment.SpecialFolder.System);
+        private const string sysDirString = "System";
+
+        [Fact]
+        public void SpecialFolderTest()
+        {
+            foreach (var specialDirString in Enum.GetNames(typeof(Environment.SpecialFolder))) {
+                var folder = (Environment.SpecialFolder)Enum.Parse(typeof(Environment.SpecialFolder), specialDirString);
+
+                AssertLayoutRendererOutput(string.Format("${{specialfolder:folder={0}}}", specialDirString), Environment.GetFolderPath(folder));
+            }
+        }
+
+        [Fact]
+        public void SpecialFolderDirCombineTest()
+        {
+            AssertLayoutRendererOutput(string.Format("${{specialfolder:folder={0}:dir=aaa}}", sysDirString), Path.Combine(this.sysDir, "aaa"));
+        }
+
+        [Fact]
+        public void SpecialFolderFileCombineTest()
+        {
+            AssertLayoutRendererOutput(string.Format("${{specialfolder:folder={0}:file=aaa.txt}}", sysDirString), Path.Combine(this.sysDir, "aaa.txt"));
+        }
+
+        [Fact]
+        public void SpecialFolderDirFileCombineTest()
+        {
+            AssertLayoutRendererOutput(string.Format("${{specialfolder:folder={0}:dir=aaa:file=bbb.txt}}", sysDirString), Path.Combine(this.sysDir, "aaa", "bbb.txt"));
+        }
+    }
+}
+#endif
