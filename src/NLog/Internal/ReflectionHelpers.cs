477fb3528a424c988c00cb9a3d6c41fc1202ea51
stefandevo <stefan.de.vogelaere@gmail.com>
2014-07-17 07:44:23 +0200

    Bugfix load renderers

--- src/NLog/Internal/ReflectionHelpers.cs
+++ src/NLog/Internal/ReflectionHelpers.cs
@@ -57,7 +57,7 @@ namespace NLog.Internal
 		/// <remarks>Types which cannot be loaded are skipped.</remarks>
 		public static Type[] SafeGetTypes(this Assembly assembly)
 		{
-#if SILVERLIGHT
+#if SILVERLIGHT && !WINDOWS_PHONE
             return assembly.GetTypes();
 #else
 			try
