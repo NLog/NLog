509756b534fe36689ee4527625589fdbded636a2
304NotModified <304NotModified@users.noreply.github.com>
2015-05-19 22:23:33 +0200

    removed old tools, new SHFB config
    - removed old tools
    - removed old configs
    - created new config for NLog 4.0

--- src/Docs/BuildDoc.cmd
+++
@@ -1,7 +0,0 @@
-rem @echo off
-set FRAMEWORK=%1
-if (%1)==() set FRAMEWORK=".NET Framework 3.5"
-set CONFIGURATION=%2
-if (%2)==() set CONFIGURATION=Debug
-%WINDIR%\Microsoft.NET\Framework\v3.5\MSBuild.exe %~dp0NLog.shfbproj /p:Framework=%FRAMEWORK%
-
--- tools/Sandcastle/Examples/setPath.bat
+++
@@ -1,1 +0,0 @@
-set PATH=%PATH%;%CD%\..\ProductionTools
