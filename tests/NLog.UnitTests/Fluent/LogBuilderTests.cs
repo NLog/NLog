3c489ad740438a5e90589eab1def1c8c80beb92e
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-10-18 23:46:34 +0200

    fix loggername for static calls, fix creation of fluent logger

--- tests/NLog.UnitTests/Fluent/LogBuilderTests.cs
+++ tests/NLog.UnitTests/Fluent/LogBuilderTests.cs
@@ -64,45 +64,49 @@ namespace NLog.UnitTests.Fluent
         [Fact]
         public void TraceWrite()
         {
-            TraceWrite_internal(_logger.Trace());
+            TraceWrite_internal(() => _logger.Trace());
         }
 
 #if NET4_5
         [Fact]
         public void TraceWrite_static_builder()
         {
-            TraceWrite_internal(Log.Trace());
+            TraceWrite_internal(() => Log.Trace(), true);
         }
 #endif
 
 
         ///<remarks>
+        /// func because 1 logbuilder creates 1 message
+        /// 
         /// Caution: don't use overloading, that will break xUnit:
         /// CATASTROPHIC ERROR OCCURRED:
         /// System.ArgumentException: Ambiguous method named TraceWrite in type NLog.UnitTests.Fluent.LogBuilderTests
         /// </remarks>
-        private void TraceWrite_internal(LogBuilder logBuilder)
+        private void TraceWrite_internal(Func<LogBuilder> logBuilder, bool isStatic = false)
         {
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message.")
                 .Property("Test", "TraceWrite")
                 .Write();
 
+
+            var loggerName = isStatic ? "LogBuilderTests" : "logger1";
             {
-                var expectedEvent = new LogEventInfo(LogLevel.Trace, "logger1", "This is a test fluent message.");
+                var expectedEvent = new LogEventInfo(LogLevel.Trace, loggerName, "This is a test fluent message.");
                 expectedEvent.Properties["Test"] = "TraceWrite";
                 AssertLastLogEventTarget(expectedEvent);
             }
 
             var ticks = DateTime.Now.Ticks;
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message '{0}'.", ticks)
                 .Property("Test", "TraceWrite")
                 .Write();
 
             {
                 var rendered = string.Format("This is a test fluent message '{0}'.", ticks);
-                var expectedEvent = new LogEventInfo(LogLevel.Trace, "logger1", "This is a test fluent message '{0}'.");
+                var expectedEvent = new LogEventInfo(LogLevel.Trace, loggerName, "This is a test fluent message '{0}'.");
                 expectedEvent.Properties["Test"] = "TraceWrite";
                 AssertLastLogEventTarget(expectedEvent);
                 AssertDebugLastMessage("t2", rendered);
@@ -192,44 +196,47 @@ namespace NLog.UnitTests.Fluent
         [Fact]
         public void InfoWrite()
         {
-            InfoWrite_internal(_logger.Info());
+            InfoWrite_internal(() => _logger.Info());
         }
 
 #if NET4_5
         [Fact]
         public void InfoWrite_static_builder()
         {
-            InfoWrite_internal(Log.Info());
+            InfoWrite_internal(() => Log.Info(), true);
         }
 #endif
 
         ///<remarks>
+        /// func because 1 logbuilder creates 1 message
+        /// 
         /// Caution: don't use overloading, that will break xUnit:
         /// CATASTROPHIC ERROR OCCURRED:
         /// System.ArgumentException: Ambiguous method named TraceWrite in type NLog.UnitTests.Fluent.LogBuilderTests
         /// </remarks>
-        private void InfoWrite_internal(LogBuilder logBuilder)
+        private void InfoWrite_internal(Func<LogBuilder> logBuilder, bool isStatic = false)
         {
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message.")
                 .Property("Test", "InfoWrite")
                 .Write();
 
+            var loggerName = isStatic ? "LogBuilderTests" : "logger1";
             {
                 //previous
-                var expectedEvent = new LogEventInfo(LogLevel.Info, "logger1", "This is a test fluent message.");
+                var expectedEvent = new LogEventInfo(LogLevel.Info, loggerName, "This is a test fluent message.");
                 expectedEvent.Properties["Test"] = "InfoWrite";
                 AssertLastLogEventTarget(expectedEvent);
             }
 
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message '{0}'.", DateTime.Now.Ticks)
                 .Property("Test", "InfoWrite")
                 .Write();
 
             {
                 //previous
-                var expectedEvent = new LogEventInfo(LogLevel.Info, "logger1", "This is a test fluent message '{0}'.");
+                var expectedEvent = new LogEventInfo(LogLevel.Info, loggerName, "This is a test fluent message '{0}'.");
                 expectedEvent.Properties["Test"] = "InfoWrite";
                 AssertLastLogEventTarget(expectedEvent);
                 AssertDebugLastMessageContains("t2", "This is a test fluent message '");
@@ -239,51 +246,53 @@ namespace NLog.UnitTests.Fluent
         [Fact]
         public void DebugWrite()
         {
-            ErrorWrite_internal(_logger.Debug(), LogLevel.Debug);
+            ErrorWrite_internal(() => _logger.Debug(), LogLevel.Debug);
         }
 
 #if NET4_5
         [Fact]
         public void DebugWrite_static_builder()
         {
-            ErrorWrite_internal(Log.Debug(), LogLevel.Debug);
+            ErrorWrite_internal(() => Log.Debug(), LogLevel.Debug, true);
         }
 #endif
 
         [Fact]
         public void FatalWrite()
         {
-            ErrorWrite_internal(_logger.Fatal(), LogLevel.Fatal);
+            ErrorWrite_internal(() => _logger.Fatal(), LogLevel.Fatal);
         }
 
 #if NET4_5
         [Fact]
         public void FatalWrite_static_builder()
         {
-            ErrorWrite_internal(Log.Fatal(), LogLevel.Fatal);
+            ErrorWrite_internal(() => Log.Fatal(), LogLevel.Fatal, true);
         }
 #endif
 
         [Fact]
         public void ErrorWrite()
         {
-            ErrorWrite_internal(_logger.Error(), LogLevel.Error);
+            ErrorWrite_internal(() => _logger.Error(), LogLevel.Error);
         }
 
 #if NET4_5
         [Fact]
         public void ErrorWrite_static_builder()
         {
-            ErrorWrite_internal(Log.Error(), LogLevel.Error);
+            ErrorWrite_internal(() => Log.Error(), LogLevel.Error, true);
         }
 #endif
 
         ///<remarks>
+        /// func because 1 logbuilder creates 1 message
+        /// 
         /// Caution: don't use overloading, that will break xUnit:
         /// CATASTROPHIC ERROR OCCURRED:
         /// System.ArgumentException: Ambiguous method named TraceWrite in type NLog.UnitTests.Fluent.LogBuilderTests
         /// </remarks>
-        private void ErrorWrite_internal(LogBuilder logBuilder, LogLevel logLevel)
+        private void ErrorWrite_internal(Func<LogBuilder> logBuilder, LogLevel logLevel, bool isStatic = false)
         {
             Exception catchedException = null;
             string path = "blah.txt";
@@ -295,38 +304,39 @@ namespace NLog.UnitTests.Fluent
             catch (Exception ex)
             {
                 catchedException = ex;
-                logBuilder
+                logBuilder()
                     .Message("Error reading file '{0}'.", path)
                     .Exception(ex)
                     .Property("Test", "ErrorWrite")
                     .Write();
             }
 
+            var loggerName = isStatic ? "LogBuilderTests" : "logger1";
             {
-                var expectedEvent = new LogEventInfo(logLevel, "logger1", "Error reading file '{0}'.");
+                var expectedEvent = new LogEventInfo(logLevel, loggerName, "Error reading file '{0}'.");
                 expectedEvent.Properties["Test"] = "ErrorWrite";
                 expectedEvent.Exception = catchedException;
                 AssertLastLogEventTarget(expectedEvent);
                 AssertDebugLastMessageContains("t2", "Error reading file '");
             }
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message.")
                 .Property("Test", "ErrorWrite")
                 .Write();
 
             {
-                var expectedEvent = new LogEventInfo(logLevel, "logger1", "This is a test fluent message.");
+                var expectedEvent = new LogEventInfo(logLevel, loggerName, "This is a test fluent message.");
                 expectedEvent.Properties["Test"] = "ErrorWrite";
                 AssertLastLogEventTarget(expectedEvent);
             }
 
-            logBuilder
+            logBuilder()
                 .Message("This is a test fluent message '{0}'.", DateTime.Now.Ticks)
                 .Property("Test", "ErrorWrite")
                 .Write();
 
             {
-                var expectedEvent = new LogEventInfo(logLevel, "logger1", "This is a test fluent message '{0}'.");
+                var expectedEvent = new LogEventInfo(logLevel, loggerName, "This is a test fluent message '{0}'.");
                 expectedEvent.Properties["Test"] = "ErrorWrite";
                 AssertLastLogEventTarget(expectedEvent);
                 AssertDebugLastMessageContains("t2", "This is a test fluent message '");
