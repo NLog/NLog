da348110bb56afbea28b492baf1876cc02ca86da
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-10-28 23:32:56 +0100

    fix file header

--- src/NLog/Time/CachedTimeSource.cs
+++ src/NLog/Time/CachedTimeSource.cs
@@ -10,7 +10,6 @@
 // * Redistributions of source code must retain the above copyright notice, 
 //   this list of conditions and the following disclaimer. 
 // 
-// 
 // * Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution. 
@@ -31,6 +30,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
+
 
 namespace NLog.Time
 {
