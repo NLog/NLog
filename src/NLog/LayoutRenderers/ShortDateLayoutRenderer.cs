e87762419115f106a0fb61fddd41f74e3bc977db
Alex Gil <epignosisx@gmail.com>
2016-01-02 19:16:40 -0500

    cleaned using statements

--- src/NLog/LayoutRenderers/ShortDateLayoutRenderer.cs
+++ src/NLog/LayoutRenderers/ShortDateLayoutRenderer.cs
@@ -31,11 +31,10 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
 
-using System;
-using System.Threading;
 
 namespace NLog.LayoutRenderers
 {
+    using System;
     using System.ComponentModel;
     using System.Globalization;
     using System.Text;
