6c9d7a5a4fcd4fc4890fa8e2f562b67c5fca2042
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-20 18:14:40 +0100

    NLog running on the DNX environment

---
+++ examples/NLogDotNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NlogDotNetCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NlogDotNetCoreSample")]
+[assembly: AssemblyCopyright("Copyright © Lukasz Pyrzyk 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06ed75b7-ee2e-4f1a-8f2c-b7fcd3ab94e9")]
