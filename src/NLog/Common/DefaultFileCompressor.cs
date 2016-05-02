babaf73e549792245c99343377c60e0da6e4135c
Andre Gleichner <andre.gleichner@gmail.com>
2016-05-02 17:26:16 +0200

    Made DefaultFileCompressor compile for .Net 4.5 and added IFileCompressor to other platforms

--- src/NLog/Common/DefaultFileCompressor.cs
+++ src/NLog/Common/DefaultFileCompressor.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NLog.Common
+﻿namespace NLog.Common
 {
+#if NET4_5
+using System.IO;
+using System.IO.Compression;
+#endif
+
     /// <summary>
     /// Uses .Net4.5 ZipArchive
     /// </summary>
