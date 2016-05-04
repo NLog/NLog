a02c2c4775e8056f296012af9f74d6fb543a4882
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-05-05 00:41:36 +0200

    Parse list from comma separated value

--- tests/NLog.UnitTests/Layouts/SimpleLayoutParserTests.cs
+++ tests/NLog.UnitTests/Layouts/SimpleLayoutParserTests.cs
@@ -31,6 +31,18 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
 
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NLog.Config;
+using NLog.Filters;
+#if !SILVERLIGHT 
+using Xunit.Extensions;
+#endif
+
 namespace NLog.UnitTests.Layouts
 {
     using NLog.LayoutRenderers;
@@ -499,6 +511,86 @@ namespace NLog.UnitTests.Layouts
             var le = LogEventInfo.Create(LogLevel.Info, "logger", "message");
             Assert.Equal("1/Log_{#}.log", l.Render(le));
         }
+#if !SILVERLIGHT
+        
+        [Theory]
+        [InlineData("2,3,4", "numbers", "2-3-4")]
+        [InlineData("a,b,c", "Strings", "a-b-c")]
+        [InlineData("a,b,c", "Objects", "a-b-c")]
+        [InlineData("a,,b,c", "Strings", "a--b-c")]
+        [InlineData("2.0,3.0,4.0", "doubles", "2-3-4")]
+        [InlineData("2.1,3.2,4.3", "doubles", "2.1-3.2-4.3")]
+        [InlineData("Ignore,Neutral,Ignore", "enums", "Ignore-Neutral-Ignore")]
+        [InlineData("ASCII,ISO-8859-1, UTF-8", "encodings", "System.Text.ASCIIEncoding-System.Text.Latin1Encoding-System.Text.UTF8Encoding")]
+        [InlineData("ASCII,ISO-8859-1,UTF-8", "encodings", "System.Text.ASCIIEncoding-System.Text.Latin1Encoding-System.Text.UTF8Encoding")]
+        public void LayoutWithListParamTest(string input, string propname, string expected)
+        {
+            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("layoutrenderer-with-list", typeof(LayoutRendererWithListParam));
+            SimpleLayout l = string.Format(@"${{layoutrenderer-with-list:{0}={1}}}", propname, input);
 
+            var le = LogEventInfo.Create(LogLevel.Info, "logger", "message");
+            var actual = l.Render(le);
+            Assert.Equal(expected, actual);
+        }
+#endif
+        [Fact]
+        public void LayoutWithListParamTest_listint_empty()
+        {
+            //note flags enum already supported
+
+            //can;t convert empty to int
+            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("layoutrenderer-with-list", typeof(LayoutRendererWithListParam));
+            Assert.Throws<NLogConfigurationException>(() =>
+            {
+                SimpleLayout l = @"${layoutrenderer-with-list:numbers=2,,3,4}";
+            });
+
+
+        }
+
+
+        private class LayoutRendererWithListParam : LayoutRenderer
+        {
+            public List<double> Doubles { get; set; }
+
+            public List<Layout> Layouts { get; set; }
+
+            public List<FilterResult> Enums { get; set; }
+
+            public List<int> Numbers { get; set; }
+
+            public List<string> Strings { get; set; }
+
+            public List<object> Objects { get; set; }
+
+            public List<Encoding> Encodings { get; set; }
+
+            /// <summary>
+            /// Renders the specified environmental information and appends it to the specified <see cref="StringBuilder" />.
+            /// </summary>
+            /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+            /// <param name="logEvent">Logging event.</param>
+            protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+            {
+                Append(builder, Strings);
+                AppendFormattable(builder, Numbers);
+                AppendFormattable(builder, Enums);
+                AppendFormattable(builder, Doubles);
+                Append(builder, Encodings);
+                Append(builder, Layouts);
+                Append(builder, Objects);
+            }
+
+            private void Append<T>(StringBuilder builder, IEnumerable<T> items)
+            {
+                if (items != null) builder.Append(string.Join("-", items.ToArray()));
+            }
+
+            private void AppendFormattable<T>(StringBuilder builder, IEnumerable<T> items)
+                where T : IFormattable
+            {
+                if (items != null) builder.Append(string.Join("-", items.Select(it => it.ToString(null, CultureInfo.InvariantCulture)).ToArray()));
+            }
+        }
     }
 }
