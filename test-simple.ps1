b10ad61c15987712bc9ac0fa83eed80289956ba0
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-06-03 00:31:03 +0200

    Enabled net45 unit tests, move to PS1 for dir test

---
+++ test-simple.ps1
@@ -0,0 +1,11 @@
+dotnet restore .\tests\SampleExtensions\
+dotnet restore .\tests\NLog.UnitTests\
+dotnet build .\tests\SampleExtensions --configuration release 
+dotnet build .\tests\NLog.UnitTests\ --configuration release 
+dotnet test .\tests\NLog.UnitTests\ -f netcoreapp1.0
+dir .\tests\NLog.UnitTests\bin\Release\net45
+
+$dir =  @(get-item .\tests\NLog.UnitTests\bin\Release\net45\* | ?{ $_.PSIsContainer })[0].FullName
+
+
+xunit.console.exe "${dir}c\NLog.UnitTests.dll"
