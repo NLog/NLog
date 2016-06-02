2c4b12ae832d4667c52281b8a1d5780d67b7c915
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-06-02 23:29:16 +0200

    build all

--- build-simple.bat
+++ build-simple.bat
@@ -1,2 +1,6 @@
 call dotnet restore .\src\NLog\ 
+call dotnet restore .\src\NLog.Extended\ 
+call dotnet restore .\src\NLogAutoLoadExtension\ 
 call dotnet pack .\src\NLog\  --configuration release 
+call dotnet build .\src\NLog.Extended\  --configuration release 
+call dotnet build .\src\NLogAutoLoadExtension\  --configuration release 
