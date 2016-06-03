4c31a95f05016ad28800846bb322e9bafdcbcc0f
Jens Peters <jp7677@gmail.com>
2016-06-03 10:44:56 +0200

    More unit tests.

--- tests/NLog.UnitTests/Targets/AnsiConsoleColorFormatterTests.cs
+++ tests/NLog.UnitTests/Targets/AnsiConsoleColorFormatterTests.cs
@@ -41,14 +41,38 @@ namespace NLog.UnitTests.Targets
     public class AnsiConsoleColorFormatterTests
     {
         [Theory]
-        [InlineData("This is a warning message", ConsoleOutputColor.NoChange, ConsoleOutputColor.NoChange, "This is a warning message")]
-        [InlineData("This is a warning message", ConsoleOutputColor.DarkRed, ConsoleOutputColor.NoChange, "\x1B[31mThis is a warning message\x1B[39m")]
-        [InlineData("This is a warning message", ConsoleOutputColor.Red, ConsoleOutputColor.NoChange, "\x1B[91mThis is a warning message\x1B[39m")]
-        [InlineData("This is a warning message", ConsoleOutputColor.NoChange, ConsoleOutputColor.DarkRed, "\x1B[41mThis is a warning message\x1B[0m")]
-        [InlineData("This is a warning message", ConsoleOutputColor.NoChange, ConsoleOutputColor.Red, "\x1B[101mThis is a warning message\x1B[0m")]
-        [InlineData("This is a warning message", ConsoleOutputColor.Blue, ConsoleOutputColor.DarkBlue, "\x1B[44m\x1B[94mThis is a warning message\x1B[39m\x1B[0m")]
-        [InlineData("This is a warning message", ConsoleOutputColor.DarkBlue, ConsoleOutputColor.Blue, "\x1B[104m\x1B[34mThis is a warning message\x1B[39m\x1B[0m")]
+        [InlineData("This is a message", ConsoleOutputColor.NoChange, ConsoleOutputColor.NoChange, "This is a message")]
+        [InlineData("This is a message", ConsoleOutputColor.DarkRed, ConsoleOutputColor.NoChange, "\x1B[31mThis is a message\x1B[39m")]
+        [InlineData("This is a message", ConsoleOutputColor.Red, ConsoleOutputColor.NoChange, "\x1B[91mThis is a message\x1B[39m")]
+        [InlineData("This is a message", ConsoleOutputColor.NoChange, ConsoleOutputColor.DarkRed, "\x1B[41mThis is a message\x1B[0m")]
+        [InlineData("This is a message", ConsoleOutputColor.NoChange, ConsoleOutputColor.Red, "\x1B[101mThis is a message\x1B[0m")]
+        [InlineData("This is a message", ConsoleOutputColor.Blue, ConsoleOutputColor.DarkBlue, "\x1B[44m\x1B[94mThis is a message\x1B[39m\x1B[0m")]
+        [InlineData("This is a message", ConsoleOutputColor.DarkBlue, ConsoleOutputColor.Blue, "\x1B[104m\x1B[34mThis is a message\x1B[39m\x1B[0m")]
         public void RowHighlightingTextTest(string message, ConsoleOutputColor foregroundColor, ConsoleOutputColor backgroundColor, string expectedMessage)
+        {
+            var rule = new ConsoleRowHighlightingRule { ForegroundColor = foregroundColor, BackgroundColor = backgroundColor };
+            
+            var formattedMessage = AnsiConsoleColorFormatter.FormatRow(message, rule);
+            
+            Assert.Equal(expectedMessage, formattedMessage);
+        }
+        
+        [Theory]
+        [InlineData("This is a message", ConsoleOutputColor.Gray, ConsoleOutputColor.NoChange, "\x1B[37mThis is a message\x1B[39m")]
+        [InlineData("This is a message", ConsoleOutputColor.NoChange, ConsoleOutputColor.Gray, "\x1B[47mThis is a message\x1B[0m")]
+        public void GrayHasDarkWhiteAnsiCodeTest(string message, ConsoleOutputColor foregroundColor, ConsoleOutputColor backgroundColor, string expectedMessage)
+        {
+            var rule = new ConsoleRowHighlightingRule { ForegroundColor = foregroundColor, BackgroundColor = backgroundColor };
+            
+            var formattedMessage = AnsiConsoleColorFormatter.FormatRow(message, rule);
+            
+            Assert.Equal(expectedMessage, formattedMessage);
+        }
+        
+        [Theory]
+        [InlineData("This is a message", ConsoleOutputColor.DarkGray, ConsoleOutputColor.NoChange, "\x1B[90mThis is a message\x1B[39m")]
+        [InlineData("This is a message", ConsoleOutputColor.NoChange, ConsoleOutputColor.DarkGray, "\x1B[100mThis is a message\x1B[0m")]
+        public void DarkGrayHasBrightBlackAnsiCodeTest(string message, ConsoleOutputColor foregroundColor, ConsoleOutputColor backgroundColor, string expectedMessage)
         {
             var rule = new ConsoleRowHighlightingRule { ForegroundColor = foregroundColor, BackgroundColor = backgroundColor };
             
