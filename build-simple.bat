7faa7475aa54d991d93d0d5b936ab2545fbc07ce
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-06-27 23:33:22 +0200

    Added docs to build script

--- build-simple.bat
+++ build-simple.bat
@@ -1,3 +1,5 @@
+rem restore and builds all projects as release.
+rem creates NuGet package at \src\NLog\bin\Release
 call dotnet restore .\src\NLog\ 
 call dotnet restore .\src\NLog.Extended\ 
 call dotnet restore .\src\NLogAutoLoadExtension\ 
