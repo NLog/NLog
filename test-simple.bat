d087309f62c0b7112ad093b54f8fc22615d7e5c2
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-07-11 23:53:19 +0200

    Update test script

--- test-simple.bat
+++ test-simple.bat
@@ -1,5 +1,5 @@
 call dotnet restore .\tests\SampleExtensions\
 call dotnet restore .\tests\NLog.UnitTests\
-call dotnet build .\tests\SampleExtensions --configuration release \
-call dotnet build .\tests\NLog.UnitTests\ --configuration release 
+call dotnet build .\tests\SampleExtensions --configuration release 
+call dotnet build .\tests\NLog.UnitTests\ --configuration release
 call dotnet test .\tests\NLog.UnitTests\
