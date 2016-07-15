5a3dd7602bcf2ed12aab7cabf7c4ae5808e69ee1
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-07-15 23:15:21 +0200

    update test script

--- test-simple.ps1
+++ test-simple.ps1
@@ -2,10 +2,16 @@ dotnet restore .\tests\SampleExtensions\
 dotnet restore .\tests\NLog.UnitTests\
 dotnet build .\tests\SampleExtensions --configuration release 
 dotnet build .\tests\NLog.UnitTests\ --configuration release 
-dotnet test .\tests\NLog.UnitTests\ -f netcoreapp1.0
+
+
+write-line "start xunit .NET Core"
+dotnet test .\tests\NLog.UnitTests\ -f netcoreapp1.0  --configuration release 
+
+write-line "end xunit .NET Core"
 dir .\tests\NLog.UnitTests\bin\Release\net45
 
 $dir =  @(get-item .\tests\NLog.UnitTests\bin\Release\net45\* | ?{ $_.PSIsContainer })[0].FullName
 
-
+#version print
+xunit.console.exe
 xunit.console.exe "${dir}\NLog.UnitTests.dll"
