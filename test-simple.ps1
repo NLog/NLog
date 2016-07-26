1cba3888753f25d4f43cea4425296dc15ea0d5d7
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-07-27 00:02:17 +0200

    improve test script logging

--- test-simple.ps1
+++ test-simple.ps1
@@ -4,10 +4,10 @@ dotnet build .\tests\SampleExtensions --configuration release
 dotnet build .\tests\NLog.UnitTests\ --configuration release 
 
 
-write-output "start xunit .NET Core"
+write-output "-----start xunit .NET Core-----"
 dotnet test .\tests\NLog.UnitTests\ -f netcoreapp1.0  --configuration release 
+write-output "-----end xunit .NET Core-----"
 
-write-output "end xunit .NET Core"
 dir .\tests\NLog.UnitTests\bin\Release\net45
 dir .\tests\NLog.UnitTests\bin\Release\net45\win7-x64
 
@@ -18,4 +18,7 @@ $dir =  @(get-item .\tests\NLog.UnitTests\bin\Release\net45\* | ?{ $_.PSIsContai
 
 #version print
 .\tools\xunit22\xunit.console.exe
+
+write-output "-----start xunit .NET 4.5-----"
 .\tools\xunit22\xunit.console.exe "${dir}\NLog.UnitTests.dll" 
+write-output "-----end xunit .NET 4.5-----"
