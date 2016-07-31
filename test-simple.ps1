dbaed379058c43b597c7b0671c414dcfdb87d68b
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-07-31 14:52:28 +0200

    .net 451 unit tests with xUnit 2.2

--- test-simple.ps1
+++ test-simple.ps1
@@ -3,22 +3,4 @@ dotnet restore .\tests\NLog.UnitTests\
 dotnet build .\tests\SampleExtensions --configuration release 
 dotnet build .\tests\NLog.UnitTests\ --configuration release 
 
-
-write-output "-----start xunit .NET Core-----"
-dotnet test .\tests\NLog.UnitTests\ -f netcoreapp1.0  --configuration release 
-write-output "-----end xunit .NET Core-----"
-
-dir .\tests\NLog.UnitTests\bin\Release\net45
-dir .\tests\NLog.UnitTests\bin\Release\net45\win7-x64
-
-$dir =  @(get-item .\tests\NLog.UnitTests\bin\Release\net45\* | ?{ $_.PSIsContainer })[0].FullName
-
-
-
-
-#version print
-.\tools\xunit22\xunit.console.exe
-
-write-output "-----start xunit .NET 4.5-----"
-.\tools\xunit22\xunit.console.exe "${dir}\NLog.UnitTests.dll" 
-write-output "-----end xunit .NET 4.5-----"
+dotnet test .\tests\NLog.UnitTests\  --configuration release 
