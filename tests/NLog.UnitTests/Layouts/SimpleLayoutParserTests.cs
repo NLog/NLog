02f32168a5a76bf279d1048f1e13069d90f5765a
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-08-06 00:36:15 +0200

    Invalid Layout Should parse partly

--- tests/NLog.UnitTests/Layouts/SimpleLayoutParserTests.cs
+++ tests/NLog.UnitTests/Layouts/SimpleLayoutParserTests.cs
@@ -37,6 +37,7 @@ namespace NLog.UnitTests.Layouts
     using NLog.LayoutRenderers.Wrappers;
     using NLog.Layouts;
     using NLog.Targets;
+    using System;
     using Xunit;
 
     public class SimpleLayoutParserTests : NLogTestBase
@@ -500,5 +501,24 @@ namespace NLog.UnitTests.Layouts
             Assert.Equal("1/Log_{#}.log", l.Render(le));
         }
 
+        [Fact]
+        public void InvalidLayoutWillParsePartly()
+        {
+            SimpleLayout l = @"aaa ${iDontExist} bbb";
+
+            var le = LogEventInfo.Create(LogLevel.Info, "logger", "message");
+            Assert.Equal("aaa  bbb", l.Render(le));
+        }
+
+        [Fact]
+        public void InvalidLayoutWillThrowIfExceptionThrowingIsOn()
+        {
+            LogManager.ThrowConfigExceptions = true;
+            Assert.Throws<ArgumentException>(() =>
+            {
+                SimpleLayout l = @"aaa ${iDontExist} bbb";
+            });
+
+        }
     }
 }
