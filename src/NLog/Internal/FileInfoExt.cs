8e09233cb0df8112027928726219df2fd6913727
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-08-07 02:08:39 +0200

    WIP: extension instead of repetive code

---
+++ src/NLog/Internal/FileInfoExt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Internal
+{
+    internal static class FileInfoExt
+    {
+        public static DateTime GetLastWriteTimeUtc(this FileInfo fileInfo)
+        {
+#if !SILVERLIGHT
+            return fileInfo.LastWriteTimeUtc;
+#else
+            return fileInfo.LastWriteTime;
+#endif
+        }
+        public static DateTime GetCreationTimeUtc(this FileInfo fileInfo)
+        {
+#if !SILVERLIGHT
+            return fileInfo.CreationTimeUtc;
+#else
+            return fileInfo.CreationTime;
+#endif
+        }
+
+    }
+}
