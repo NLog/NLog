d278a4f37f8ccb279673175909910d44c78415a0
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-09-11 23:25:03 +0200

    ticks test

---
+++ tests/NLog.UnitTests/LayoutRenderers/TicksLayoutRendererTests.cs
@@ -0,0 +1,78 @@
+// 
+// Copyright (c) 2004-2016 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.LayoutRenderers;
+using Xunit;
+using Xunit.Extensions;
+using NLog.Layouts;
+
+namespace NLog.UnitTests.LayoutRenderers
+{
+    public class TicksLayoutRendererTests : NLogTestBase
+    {
+        [Fact]
+        public void RenderTicksLayoutRenderer()
+        {
+            Layout layout = "${ticks}";
+
+            layout.Initialize(null);
+            var now = DateTime.Now;
+            var currentTicks = now.Ticks;
+            var logEventInfo = LogEventInfo.CreateNullEvent();
+            logEventInfo.TimeStamp = now;
+            string actual = layout.Render(logEventInfo);
+            layout.Close();
+
+            Assert.NotNull(actual);
+
+            long number;
+            if (!long.TryParse(actual, out number))
+            {
+                Assert.True(false, "ticks is not a number");
+            }
+
+            Assert.Equal(currentTicks, number);
+            Assert.True(number > 0, "number > 0");
+        }
+
+      
+    }
+}
