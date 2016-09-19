06bab62f660143bb45846a1c002125603a64326f
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-09-19 22:49:11 +0200

    print dotnet version

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 # restore and builds all projects as release.
 # creates NuGet package at \artifacts
+dotnet --version
+
 dotnet restore .\src\NLog\ 
 dotnet restore .\src\NLog.Extended\ 
 dotnet restore .\src\NLogAutoLoadExtension\ 
