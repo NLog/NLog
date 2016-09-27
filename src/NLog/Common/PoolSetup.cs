2d13f9373434fa5a9a8a118762f9c748b6b49d72
Rolf Kristensen <sweaty1@hotmail.com>
2016-09-27 23:18:29 +0200

    PoolSetup - Added documentation (Retry)

--- src/NLog/Common/PoolSetup.cs
+++ src/NLog/Common/PoolSetup.cs
@@ -1,4 +1,4 @@
-﻿//
+﻿// 
 // Copyright (c) 2004-2016 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
 // 
 // All rights reserved.
@@ -31,10 +31,10 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
 
-using System;
-
 namespace NLog.Common
 {
+    using System;
+
     /// <summary>
     /// Object Pool Configuration for Target and Logger
     /// </summary>
