8e08d70381d2e34495123e06029eaa6f492274c2
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-10-13 09:33:04 +0200

    disable EventLogTargetTests for NETSTANDARD

--- tests/NLog.UnitTests/Targets/EventLogTargetTests.cs
+++ tests/NLog.UnitTests/Targets/EventLogTargetTests.cs
@@ -31,7 +31,7 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
 
-#if !SILVERLIGHT && !NETSTANDARD && !MONO && !NETSTANDARD_1plus
+#if !SILVERLIGHT && !NETSTANDARD && !MONO && !NETSTANDARD
 
 namespace NLog.UnitTests.Targets
 {
