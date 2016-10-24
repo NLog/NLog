b841a6a547367da5d95a282f5c4158daf87d295e
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-10-24 23:29:39 +0200

    nuget for mono2 [skip appveyor]

--- build-mono.sh
+++ build-mono.sh
@@ -2,8 +2,10 @@
 set -ev
 
 if [ "$MONO_V" = "MONO2" ]; then
-  nuget restore src/NLog.mono2.sln
-  nuget install xunit.runners -Version 1.9.2 -OutputDirectory testrunner
+
+  PATH=$TRAVIS_BUILD_DIR/src/.nuget:$PATH 
+  mono --runtime=v4.0 src/.nuget/NuGet.exe restore src/NLog.mono2.sln
+  mono --runtime=v4.0 src/.nuget/NuGet.exe install xunit.runners -Version 1.9.2 -OutputDirectory testrunner
   xbuild /p:Configuration=Release src/NLog.mono2.sln
   mono ./testrunner/xunit.runners.1.9.2/tools/xunit.console.exe      "./build/bin/Release/Mono 2.x/NLog.UnitTests.dll"
 fi
