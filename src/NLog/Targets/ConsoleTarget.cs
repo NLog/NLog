882e895876bf4e7f87d64cacb3e8f28b9425f479
Rolf Kristensen <snakefoot@users.noreply.github.com>
2016-12-18 20:56:00 +0100

    ConsoleTarget - Apply Encoding on InitializeTarget, if Console available (#1850)
    * ConsoleTarget - Encoding is applied on InitializeTarget, if Console is available
    
    * ConsoleTarget - Encoding is applied on InitializeTarget, if Console is available (Test)
    
    * ConsoleTarget - Encoding is applied on InitializeTarget, if Console is available (Fix)
    
    * ConsoleTarget - Encoding is applied on InitializeTarget, if Console is available (Review comments)

--- src/NLog/Targets/ConsoleTarget.cs
+++ src/NLog/Targets/ConsoleTarget.cs
@@ -77,7 +77,7 @@ namespace NLog.Targets
         ///   TextWriter's Synchronized methods.This also applies to classes like StreamWriter and StreamReader.
         /// 
         /// </remarks>
-        private bool PauseLogging;
+        private bool _pauseLogging;
 
         /// <summary>
         /// Gets or sets a value indicating whether to send the log messages to the standard error instead of the standard output.
@@ -93,9 +93,17 @@ namespace NLog.Targets
         /// <remarks>Has side effect</remarks>
         public Encoding Encoding
         {
-            get { return Console.OutputEncoding; }
-            set { Console.OutputEncoding = value; }
+            get
+            {
+                return ConsoleTargetHelper.GetConsoleOutputEncoding(_encoding, IsInitialized, _pauseLogging);
+            }
+            set
+            {
+                if (ConsoleTargetHelper.SetConsoleOutputEncoding(value, IsInitialized, _pauseLogging))
+                    _encoding = value;
+            }
         }
+        private Encoding _encoding;
 #endif
 
         /// <summary>
@@ -114,7 +122,7 @@ namespace NLog.Targets
         /// </remarks>
         public ConsoleTarget() : base()
         {
-            PauseLogging = false;
+            _pauseLogging = false;
             DetectConsoleAvailable = false;
         }
 
@@ -136,16 +144,20 @@ namespace NLog.Targets
         /// </summary>
         protected override void InitializeTarget()
         {
-            PauseLogging = false;
+            _pauseLogging = false;
             if (DetectConsoleAvailable)
             {
                 string reason;
-                PauseLogging = !ConsoleTargetHelper.IsConsoleAvailable(out reason);
-                if (PauseLogging)
+                _pauseLogging = !ConsoleTargetHelper.IsConsoleAvailable(out reason);
+                if (_pauseLogging)
                 {
                     InternalLogger.Info("Console has been detected as turned off. Disable DetectConsoleAvailable to skip detection. Reason: {0}", reason);
                 }
             }
+#if !SILVERLIGHT && !__IOS__ && !__ANDROID__
+            if (_encoding != null && !_pauseLogging)
+                Console.OutputEncoding = _encoding;
+#endif
             base.InitializeTarget();
             if (Header != null)
             {
@@ -176,7 +188,7 @@ namespace NLog.Targets
         /// </remarks>
         protected override void Write(LogEventInfo logEvent)
         {
-            if (PauseLogging)
+            if (_pauseLogging)
             {
                 //check early for performance
                 return;
@@ -191,7 +203,7 @@ namespace NLog.Targets
         /// <param name="textLine">text to be written.</param>
         private void WriteToOutput(string textLine)
         {
-            if (PauseLogging)
+            if (_pauseLogging)
             {
                 return;
             }
@@ -205,7 +217,7 @@ namespace NLog.Targets
             catch (IndexOutOfRangeException ex)
             {
                 //this is a bug and therefor stopping logging. For docs, see PauseLogging property
-                PauseLogging = true;
+                _pauseLogging = true;
                 InternalLogger.Warn(ex, "An IndexOutOfRangeException has been thrown and this is probably due to a race condition." +
                                         "Logging to the console will be paused. Enable by reloading the config or re-initialize the targets");
             }
