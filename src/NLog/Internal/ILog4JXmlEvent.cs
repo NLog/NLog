9b542fa18b5c0a0542c9e08147a756d476c943f1
nikola02333 <nikolaniki640@gmail.com>
2017-05-17 16:49:46 +0200

    ILog4JXmlEvent
    ILog4JXmlEvent  interface for  Log4JXmlEventLayout code improvements

---
+++ src/NLog/Internal/ILog4JXmlEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Internal
+{
+    interface ILog4JXmlEvent
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to include contents of the <see cref="MappedDiagnosticsContext"/> dictionary.
+        /// </summary>
+        bool IncludeMdc { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to include contents of the <see cref="MappedDiagnosticsLogicalContext"/> dictionary.
+        /// </summary>
+        bool IncludeMdlc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the option to include all properties from the log events
+        /// </summary>
+        bool IncludeAllProperties { get; set; }
+    }
+}
