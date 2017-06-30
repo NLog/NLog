9ea5a94b4555df5157c2e75e1f499602bd5877e6
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-06-30 23:42:10 +0200

    Internal Util classes to static classes

--- src/NLog/Internal/StackTraceUsageUtils.cs
+++ src/NLog/Internal/StackTraceUsageUtils.cs
@@ -40,7 +40,7 @@ namespace NLog.Internal
     /// <summary>
     /// Utilities for dealing with <see cref="StackTraceUsage"/> values.
     /// </summary>
-    internal class StackTraceUsageUtils
+    internal static class StackTraceUsageUtils
     {
         internal static StackTraceUsage Max(StackTraceUsage u1, StackTraceUsage u2)
         {
