7d642fc7d879ab433c0232b9c023ed8e39f94c81
rbarillec <rbarillec@gmail.com>
2017-07-13 19:40:23 +0100

    Updated SQLite database URI to be compatible with Mono

--- tests/NLog.UnitTests/Targets/DatabaseTargetTests.cs
+++ tests/NLog.UnitTests/Targets/DatabaseTargetTests.cs
@@ -992,7 +992,7 @@ Dispose()
 
             // Use an in memory SQLite database
             // See https://www.sqlite.org/inmemorydb.html
-            var connectionString = String.Format("FullUri=file:{0}?mode=memory;Version=3", databaseName);
+            var connectionString = String.Format("Uri=file::memory:;Version=3", databaseName);
 
             LogManager.Configuration = CreateConfigurationFromString(
                 String.Format(nlogXmlConfig, GetSQLiteDbProvider(), connectionString)
