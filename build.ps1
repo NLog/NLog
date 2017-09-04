269eb7d2db7ad3789030cd84787e71f4f08b6640
Rolf Kristensen <sweaty1@hotmail.com>
2017-09-05 00:45:51 +0200

    VS2017 - NetCore2 (All targetframeworks)

--- build.ps1
+++ build.ps1
@@ -2,7 +2,10 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-dotnet restore .\src\NLog\
-dotnet pack .\src\NLog\  --configuration release -o artifacts
+# dotnet restore .\src\NLog\
+msbuild /t:Restore /p:targetFrameworks='"net45;net40;net35;netstandard2.0;sl4;sl5;wp8;monoandroid44;xamarinios10"' .\src\NLog\
+
+# dotnet pack .\src\NLog\  --configuration release --include-symbols -o ..\..\artifacts
+msbuild /t:Pack /p:targetFrameworks='"net45;net40;net35;netstandard2.0;sl4;sl5;wp8;monoandroid44;xamarinios10"' /p:Configuration=Release /p:IncludeSymbols=true .\src\NLog\ /p:PackageOutputPath=..\..\artifacts
 
 exit $LASTEXITCODE
--- run-tests.ps1
+++ run-tests.ps1
@@ -1,8 +1,10 @@
 dotnet restore .\src\NLogAutoLoadExtension\
 dotnet restore .\tests\SampleExtensions\
 dotnet restore .\tests\NLog.UnitTests\
+
 dotnet build .\src\NLogAutoLoadExtension\ --configuration release 
 dotnet build .\tests\SampleExtensions\ --configuration release 
+dotnet build .\tests\NLog.UnitTests\ --configuration debug
 dotnet build .\tests\NLog.UnitTests\ --configuration release 
 
 dotnet test .\tests\NLog.UnitTests\  --configuration release 
