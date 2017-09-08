47a810353e14e70b9d1861017bfeb2356b5edbe3
Rolf Kristensen <sweaty1@hotmail.com>
2017-09-09 01:44:35 +0200

    VS2017 - NetCore2 (UnitTest for Net35 and Net40-client)

--- build.ps1
+++ build.ps1
@@ -3,20 +3,12 @@
 dotnet --version
 
 # dotnet restore .\src\NLog\
-msbuild /t:Restore /p:targetFrameworks='"net45;net40-client;net35;netstandard2.0;sl4;sl5;wp8;monoandroid44;xamarinios10"' .\src\NLog\ /verbosity:minimal
-if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
-
 # dotnet pack .\src\NLog\  --configuration release --include-symbols -o ..\..\artifacts
-msbuild /t:Pack /p:targetFrameworks='"net45;net40-client;net35;netstandard2.0;sl4;sl5;wp8;monoandroid44;xamarinios10"' /p:Configuration=Release /p:IncludeSymbols=true .\src\NLog\ /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
+msbuild /t:Restore,Pack /p:targetFrameworks='"net45;net40-client;net35;netstandard2.0;sl4;sl5;wp8;monoandroid44;xamarinios10"' /p:Configuration=Release /p:IncludeSymbols=true .\src\NLog\ /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 
-msbuild /t:Restore /p:targetFrameworks='"net45;net40-client;net35"' .\src\NLog.Extended\ /verbosity:minimal
-if (-Not $LastExitCode -eq 0)
-	{ exit $LastExitCode }
-
-msbuild /t:Pack /p:targetFrameworks='"net45;net40-client;net35"' /p:Configuration=Release /p:IncludeSymbols=true .\src\NLog.Extended\ /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
+msbuild /t:Restore,Pack /p:targetFrameworks='"net45;net40-client;net35"' /p:Configuration=Release /p:IncludeSymbols=true .\src\NLog.Extended\ /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 
--- run-tests.ps1
+++ run-tests.ps1
@@ -1,11 +1,47 @@
+dotnet restore .\src\NLog\
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
 dotnet restore .\src\NLogAutoLoadExtension\
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
 dotnet restore .\tests\SampleExtensions\
-dotnet restore .\tests\NLog.UnitTests\
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
-dotnet build .\src\NLogAutoLoadExtension\ --configuration release 
-dotnet build .\tests\SampleExtensions\ --configuration release 
-dotnet build .\tests\NLog.UnitTests\ --configuration debug
-dotnet build .\tests\NLog.UnitTests\ --configuration release 
+msbuild /t:Build /p:Configuration=Release .\src\NLogAutoLoadExtension\ /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
-dotnet test .\tests\NLog.UnitTests\  --configuration release 
-exit $LASTEXITCODE
+msbuild /t:Build /p:Configuration=Release .\tests\SampleExtensions\ /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+msbuild /t:Restore,Build /p:Configuration=Release .\tests\NLog.UnitTests\ /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+dotnet test .\tests\NLog.UnitTests\  --configuration release --framework netcoreapp2.0 --no-build
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+msbuild /t:Build /p:targetFramework=net452 .\tests\NLog.UnitTests\ /p:Configuration=Release /p:TestTargetFramework=net35 /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+& ${env:xunit20}\xunit.console.x86.exe .\tests\NLog.UnitTests\bin\release\net452\NLog.UnitTests.dll -appveyor -noshadow
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+msbuild /t:Build /p:targetFramework=net452 .\tests\NLog.UnitTests\ /p:Configuration=Release /p:TestTargetFramework=net40-client /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+& ${env:xunit20}\xunit.console.x86.exe .\tests\NLog.UnitTests\bin\release\net452\NLog.UnitTests.dll -appveyor -noshadow
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+
+msbuild /t:Build /p:Configuration=Debug .\tests\NLog.UnitTests\ /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
