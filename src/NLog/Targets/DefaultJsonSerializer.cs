b6e6fdcee1f6092e9d8a1dd33fb39b38f5ce0df7
Julian Verdurmen <j.verdurmen@2bmore.nl>
2018-01-22 12:59:00 +0100

    Docs for DefaultJsonSerializer

--- src/NLog/Targets/DefaultJsonSerializer.cs
+++ src/NLog/Targets/DefaultJsonSerializer.cs
@@ -69,6 +69,9 @@ namespace NLog.Targets
             instance = new DefaultJsonSerializer();
         }
 
+        /// <summary>
+        /// Private. Use <see cref="Instance"/>
+        /// </summary>
         private DefaultJsonSerializer()
         { }
 
