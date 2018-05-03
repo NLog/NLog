f2fc7ca6413601e07e0d0a7c6c261a057a0326d2
Julian Verdurmen <304NotModified@users.noreply.github.com>
2018-05-03 17:43:16 +0200

    branchmode sonar

--- run-sonar.ps1
+++ run-sonar.ps1
@@ -11,17 +11,19 @@ if ($env:APPVEYOR_REPO_NAME -eq $github) {
     }
  
     $preview = $false;
+    $branchMode = $false;
      
     if ($env:APPVEYOR_PULL_REQUEST_NUMBER) { 
         $preview = $true;
         Write-Output "Sonar: on PR $env:APPVEYOR_PULL_REQUEST_NUMBER"
     }
     elseif ($env:APPVEYOR_REPO_BRANCH -eq $baseBranch) {
-        Write-Output "Sonar: on branch $env:APPVEYOR_REPO_BRANCH"
+        Write-Output "Sonar: on base Branch"
     }
     else {
-        Write-Output "Sonar: not running SonarQube as this isn't a PR and we running on branch $env:APPVEYOR_REPO_BRANCH"
-        return;
+        $branchMode = $true;
+        Write-Output "Sonar: on branch $env:APPVEYOR_REPO_BRANCH"
+        
     }
 
     choco install "msbuild-sonarqube-runner" -y
@@ -29,6 +31,11 @@ if ($env:APPVEYOR_REPO_NAME -eq $github) {
     if ($preview) {
         Write-Output "Sonar: Running Sonar in preview mode for PR $env:APPVEYOR_PULL_REQUEST_NUMBER"
         SonarScanner.MSBuild.exe begin /k:"$sonarQubeId" /d:"sonar.analysis.mode=preview" /d:"sonar.github.pullRequest=$env:APPVEYOR_PULL_REQUEST_NUMBER" /d:"sonar.github.repository=$github" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=$env:sonar_token" 
+    }
+    elseif ($branchMode) {
+        $branch = $env:APPVEYOR_REPO_BRANCH;
+        Write-Output "Sonar: Running Sonar in branch mode for branch $branch"
+        SonarScanner.MSBuild.exe begin /k:"$sonarQubeId" /d:"sonar.branch.name=$branch" /d:"sonar.github.repository=$github" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=$env:sonar_token" 
     }
     else {
         Write-Output "Sonar: Running Sonar in non-preview mode, on branch $env:APPVEYOR_REPO_BRANCH"
