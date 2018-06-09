40cf43df7f15f9e552ae08afc5ea457cb45b562e
konstantin.chernyaev <konstantin.chernyaev@list.ru>
2018-06-09 20:29:58 +0300

    .net 3.5 - class Tuple

--- src/NLog/Targets/Wrappers/DistinctlyThrottlingWrapperTarget.cs
+++ src/NLog/Targets/Wrappers/DistinctlyThrottlingWrapperTarget.cs
@@ -31,7 +31,7 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 // 
 
-#define NET3_5
+//#define NET3_5
 
 
 #region usings
@@ -62,6 +62,16 @@ namespace NLog.Targets.Wrappers
 #if !NET3_5
         ConcurrentDictionary<AsyncLogEventInfo, Tuple<int, StringBuilder>> _entriesCounts;
 #else
+        class Tuple<T1, T2>
+        {
+            public T1 Item1;
+            public T2 Item2;
+            public Tuple(T1 item1, T2 item2)
+            {
+                Item2 = item2;
+                Item1 = item1;
+            }
+        }
         Dictionary<AsyncLogEventInfo, Tuple<int, StringBuilder>> _entriesCounts;
 #endif
 
