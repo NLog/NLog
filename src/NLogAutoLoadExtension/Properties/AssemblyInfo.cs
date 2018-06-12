6dc6213e97e1b4ab555bab7b4d97b9c48353b144
Alex Angas <alexangas@gmail.com>
2018-06-12 17:10:57 +0100

    AssemblyVersionLayout - Set distinct version types for use in tests

--- src/NLogAutoLoadExtension/Properties/AssemblyInfo.cs
+++ src/NLogAutoLoadExtension/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
