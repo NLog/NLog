8cc3fd09dd8d8fa538b0cc7d0b525dac9057ba8d
Rolf Kristensen <sweaty1@hotmail.com>
2018-09-11 19:48:15 +0200

    Layout Wrappers should not throw when Inner-Layout is null (Allow when with only else)

--- src/NLog/LayoutRenderers/Wrappers/WhenLayoutRendererWrapper.cs
+++ src/NLog/LayoutRenderers/Wrappers/WhenLayoutRendererWrapper.cs
@@ -62,15 +62,24 @@ namespace NLog.LayoutRenderers.Wrappers
         public Layout Else { get; set; }
 
         /// <inheritdoc/>
-        protected override void RenderInnerAndTransform(LogEventInfo logEvent, StringBuilder builder, int orgLength)
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
-            if (When == null || true.Equals(When.Evaluate(logEvent)))
+            int orgLength = builder.Length;
+            try
             {
-                Inner.RenderAppendBuilder(logEvent, builder);
+                if (When == null || true.Equals(When.Evaluate(logEvent)))
+                {
+                    Inner?.RenderAppendBuilder(logEvent, builder);
+                }
+                else if (Else != null)
+                {
+                    Else.RenderAppendBuilder(logEvent, builder);
+                }
             }
-            else if (Else != null)
+            catch
             {
-                Else.RenderAppendBuilder(logEvent, builder);
+                builder.Length = orgLength; // Rewind/Truncate on exception
+                throw;
             }
         }
 
