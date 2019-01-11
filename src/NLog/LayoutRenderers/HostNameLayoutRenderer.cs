f4254e513fe252a67f89492f08faa66b70746436
Amit Saha <amitsaha@users.noreply.github.com>
2019-01-12 10:35:24 +1100

    Implement a HostName Layout Renderer (#3071)
    Added ${HostName} Layout Renderer

---
+++ src/NLog/LayoutRenderers/HostNameLayoutRenderer.cs
@@ -0,0 +1,117 @@
+// 
+// Copyright (c) 2004-2018 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+#if !SILVERLIGHT
+
+namespace NLog.LayoutRenderers
+{
+    using System;
+    using System.Text;
+    using NLog.Common;
+    using NLog.Config;
+    using NLog.Internal;
+
+    /// <summary>
+    /// The host name that the process is running on.
+    /// </summary>
+    [LayoutRenderer("hostname")]
+    [AppDomainFixedOutput]
+    [ThreadAgnostic]
+    [ThreadSafe]
+    public class HostNameLayoutRenderer : LayoutRenderer
+    {
+        internal string HostName { get; private set; }
+
+        /// <inheritdoc/>
+        protected override void InitializeLayoutRenderer()
+        {
+            base.InitializeLayoutRenderer();
+            try
+            {
+                HostName = GetHostName();
+                if (string.IsNullOrEmpty(HostName))
+                {
+                    InternalLogger.Info("HostName is not available.");
+                }
+            }
+            catch (Exception exception)
+            {
+                InternalLogger.Error(exception, "Error getting host name.");
+                if (exception.MustBeRethrown())
+                {
+                    throw;
+                }
+
+                HostName = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets the host name and falls back to computer name if not available
+        /// </summary>
+        private static string GetHostName()
+        {
+            return TryLookupValue(() => Environment.GetEnvironmentVariable("HOSTNAME"), "HOSTNAME")
+                ?? TryLookupValue(() => System.Net.Dns.GetHostName(), "DnsHostName")
+                ?? TryLookupValue(() => EnvironmentHelper.GetMachineName(), "MachineName");
+        }
+
+        /// <summary>
+        /// Tries the lookup value.
+        /// </summary>
+        /// <param name="lookupFunc">The lookup function.</param>
+        /// <param name="lookupType">Type of the lookup.</param>
+        /// <returns></returns>
+        private static string TryLookupValue(Func<string> lookupFunc, string lookupType)
+        {
+            try
+            {
+                string lookupValue = lookupFunc()?.Trim();
+                return string.IsNullOrEmpty(lookupValue) ? null : lookupValue;
+            }
+            catch (Exception ex)
+            {
+                NLog.Common.InternalLogger.Warn(ex, "Failed to lookup {0}", lookupType);
+                return null;
+            }
+       }
+
+        /// <inheritdoc/>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            builder.Append(HostName);
+        }
+    }
+}
+
+#endif
