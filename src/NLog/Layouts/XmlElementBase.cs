6f68cb3dbb412859c23d1897542a04abe05e0d59
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-02-24 21:15:07 +0100

    back to C# 7.0 instead of 7.2

--- src/NLog/Layouts/XmlElementBase.cs
+++ src/NLog/Layouts/XmlElementBase.cs
@@ -70,14 +70,14 @@ namespace NLog.Layouts
         /// Name of the XML element
         /// </summary>
         /// <docgen category='XML Options' order='10' />
-        private protected string ElementNameInternal { get => _elementName; set => _elementName = XmlHelper.XmlConvertToElementName(value?.Trim(), true); }
+        protected string ElementNameInternal { get => _elementName; set => _elementName = XmlHelper.XmlConvertToElementName(value?.Trim(), true); }
         private string _elementName;
 
         /// <summary>
         /// Value inside the XML element
         /// </summary>
         /// <docgen category='XML Options' order='10' />
-        private protected Layout ElementValueInternal { get => _elementValueWrapper.Inner; set => _elementValueWrapper.Inner = value; }
+        protected Layout ElementValueInternal { get => _elementValueWrapper.Inner; set => _elementValueWrapper.Inner = value; }
         private readonly LayoutRenderers.Wrappers.XmlEncodeLayoutRendererWrapper _elementValueWrapper = new LayoutRenderers.Wrappers.XmlEncodeLayoutRendererWrapper();
 
         /// <summary>
