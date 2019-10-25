11bfeb953a468496e9046e596c84c2756906dcad
Rolf Kristensen <snakefoot@users.noreply.github.com>
2019-10-25 20:53:59 +0200

    LogManager.GetCurrentClassLogger performs correct fallback to StackTrace when inside lambda_method (#3642)

--- src/NLog/Internal/StackTraceUsageUtils.cs
+++ src/NLog/Internal/StackTraceUsageUtils.cs
@@ -186,7 +186,7 @@ namespace NLog.Internal
 #if !NETSTANDARD1_0
         /// <summary>
         /// Gets the fully qualified name of the class invoking the calling method, including the 
-        /// namespace but not the assembly.    
+        /// namespace but not the assembly.
         /// </summary>
         /// <param name="stackFrame">StackFrame from the calling method</param>
         /// <returns>Fully qualified class name</returns>
@@ -201,6 +201,8 @@ namespace NLog.Internal
                 var stackTrace = new StackTrace(false);
 #endif
                 className = GetClassFullName(stackTrace);
+                if (string.IsNullOrEmpty(className))
+                    className = stackFrame.GetMethod()?.Name ?? string.Empty;
             }
             return className;
         }
@@ -261,10 +263,17 @@ namespace NLog.Internal
             var method = stackFrame.GetMethod();
             if (method != null && LookupAssemblyFromStackFrame(stackFrame) != null)
             {
-                string className = GetStackFrameMethodClassName(method, true, true, true) ?? method.Name;
-                if (!string.IsNullOrEmpty(className) && !className.StartsWith("System.", StringComparison.Ordinal))
+                string className = GetStackFrameMethodClassName(method, true, true, true);
+                if (!string.IsNullOrEmpty(className))
                 {
-                    return className;
+                    if (!className.StartsWith("System.", StringComparison.Ordinal))
+                        return className;
+                }
+                else
+                {
+                    className = method.Name ?? string.Empty;
+                    if (className != "lambda_method" && className != "MoveNext")
+                        return className;
                 }
             }
 
