bb053fcabf250a3af4e90351a2a2a7a177500f87
Rolf Kristensen <sweaty1@hotmail.com>
2020-04-05 19:58:13 +0200

    ObjectReflectionCache - Added support for IReadOnlyDictionary as expando object

---
+++ tests/NLog.UnitTests/Internal/ReadOnlyExpandoTestDictionary.cs
@@ -0,0 +1,80 @@
+﻿// 
+// Copyright (c) 2004-2020 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+#if NET4_5
+
+namespace NLog.UnitTests.Internal
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class ReadOnlyExpandoTestDictionary : IReadOnlyDictionary<string, object>
+    {
+        private readonly IReadOnlyDictionary<string, object> _internal;
+
+        public ReadOnlyExpandoTestDictionary(IReadOnlyDictionary<string, object> dictionary)
+        {
+            _internal = dictionary;
+        }
+
+        public object this[string key] => _internal[key];
+
+        public IEnumerable<string> Keys => _internal.Keys;
+
+        public IEnumerable<object> Values => _internal.Values;
+
+        public int Count => _internal.Count;
+
+        public bool ContainsKey(string key)
+        {
+            return _internal.ContainsKey(key);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            return _internal.GetEnumerator();
+        }
+
+        public bool TryGetValue(string key, out object value)
+        {
+            return _internal.TryGetValue(key, out value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _internal.GetEnumerator();
+        }
+    }
+}
+
+#endif
