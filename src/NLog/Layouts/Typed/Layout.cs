9c911f3a77fcb3e905e0ceaa928569d1de2182a6
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-04-13 20:57:00 +0200

    Merge layout<T> and typedLayoutBase

--- src/NLog/Layouts/Typed/Layout.cs
+++ src/NLog/Layouts/Typed/Layout.cs
@@ -34,6 +34,9 @@
 using System;
 using System.ComponentModel;
 using System.Globalization;
+using System.Text;
+using JetBrains.Annotations;
+using NLog.Common;
 using NLog.Internal;
 
 namespace NLog.Layouts
@@ -42,19 +45,27 @@ namespace NLog.Layouts
     /// Layout with a simple value (e.g. int) or a layout which results in a simple value (e.g. ${counter})
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class Layout<T> : TypedLayoutBase<T>
+    public class Layout<T> : Layout, IRenderable<T>
     {
         private static TypeCode? _typeCode;
-        private static string _typeNamed;
+        private static readonly string _typeNamed;
+        private readonly Layout _layout;
+        private readonly T _value;
+        private readonly bool _fixedValue;
 
         /// <inheritdoc />
-        public Layout(T value) : base(value)
+        public Layout(T value)
         {
+            _value = value;
+            _fixedValue = true;
+            _layout = null;
         }
 
         /// <inheritdoc />
-        public Layout(Layout layout) : base(layout)
+        public Layout(Layout layout)
         {
+            _fixedValue = TryGetFixedValue(layout, out _value);
+            _layout = layout;
         }
 
         static Layout()
@@ -63,13 +74,14 @@ namespace NLog.Layouts
             _typeNamed = typeof(T).Name;
         }
 
+        /// <summary>
+        /// Is fixed value?
+        /// </summary>
+        public bool IsFixed => _fixedValue;
+
         #region Overrides of TypedLayout<T>
 
-        /// <inheritdoc />
-        protected override string TypedName => _typeNamed;
-
-        /// <inheritdoc />
-        protected override string ValueToString(T value, CultureInfo cultureInfo)
+        private static string ValueToString(T value, CultureInfo cultureInfo)
         {
             if (value is IConvertible convertible)
             {
@@ -79,20 +91,19 @@ namespace NLog.Layouts
             return value?.ToString();
         }
 
-        /// <inheritdoc />
-        protected override bool TryParse(string text, out T value)
+        private static bool TryParse(string text, out T value)
         {
             return TryConvertTo(text, out value);
         }
 
-        /// <inheritdoc />
-        protected override bool TryConvertTo(object raw, out T value)
+        private static bool TryConvertTo(object raw, out T value)
         {
             if (_typeCode == null)
             {
                 value = default(T);
                 return false;
             }
+
             return ValueConverter.TryConvertValue(raw, _typeCode.Value, out value);
         }
 
@@ -121,5 +132,140 @@ namespace NLog.Layouts
         }
 
         #endregion
+
+        /// <inheritdoc />
+        protected override string GetFormattedMessage(LogEventInfo logEvent)
+        {
+            if (_fixedValue)
+            {
+                if (_value == null)
+                {
+                    return null;
+                }
+
+                var text = ValueToString(_value, LoggingConfiguration.DefaultCultureInfo);
+                if (text != null)
+                {
+                    return text;
+                }
+            }
+
+            return _layout.Render(logEvent);
+        }
+
+        /// <inheritdoc cref="IRawValue" />
+        internal override bool TryGetRawValue(LogEventInfo logEvent, out object rawValue)
+        {
+            if (_fixedValue)
+            {
+                rawValue = _value;
+                return true;
+            }
+
+            if (_layout == null)
+            {
+                rawValue = null;
+                return true;
+            }
+
+            if (_layout.TryGetRawValue(logEvent, out var raw))
+            {
+                var success = TryConvertRawToValue(raw, out var i);
+                rawValue = i;
+                return success;
+            }
+
+            rawValue = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Render to value
+        /// </summary>
+        /// <returns></returns>
+        T IRenderable<T>.RenderToValue(LogEventInfo logEvent)
+        {
+            return RenderToValueInternal(logEvent, null);
+        }
+
+        /// <summary>
+        /// Render to value
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// <param name="reusableBuilder">if null, default layout render will be used</param>
+        /// <returns></returns>
+        internal T RenderToValueInternal(LogEventInfo logEvent, [CanBeNull] StringBuilder reusableBuilder)
+        {
+            if (_fixedValue)
+            {
+                return _value;
+            }
+
+            if (_layout == null)
+            {
+                return default(T);
+            }
+
+            if (_layout.TryGetRawValue(logEvent, out var raw))
+            {
+                if (TryConvertRawToValue(raw, out var value))
+                {
+                    return value;
+                }
+
+                InternalLogger.Warn("rawvalue isn't a {0} ", _typeNamed);
+            }
+
+            var text = reusableBuilder != null ? RenderAllocateBuilder(logEvent, reusableBuilder) : _layout.Render(logEvent);
+            if (TryParse(text, out var parsedValue))
+            {
+                return parsedValue;
+            }
+
+            InternalLogger.Warn("Parse {0} to {1} failed", text, _typeNamed);
+            return default(T);
+        }
+
+        private static bool TryConvertRawToValue(object raw, out T value)
+        {
+            if (raw == null)
+            {
+                value = default(T);
+                return true;
+            }
+
+            if (raw is T i)
+            {
+                value = i;
+                return true;
+            }
+
+            if (TryConvertTo(raw, out value))
+            {
+                return true;
+            }
+
+            value = default(T);
+
+            return false;
+        }
+
+        private static bool TryGetFixedValue(Layout layout, out T value)
+        {
+            if (layout != null && layout is SimpleLayout simpleLayout && simpleLayout.IsFixedText)
+            {
+                if (!TryParse(simpleLayout.FixedText, out value))
+                {
+                    InternalLogger.Warn("layout with text '{0}' isn't an {1}", simpleLayout.FixedText, _typeNamed);
+                }
+
+                return true;
+            }
+
+            value = default(T);
+
+            return false;
+        }
+
     }
 }
