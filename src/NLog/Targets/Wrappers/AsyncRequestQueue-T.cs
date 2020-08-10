c20597c8f1878508f21defa16bb064a9fc6de12a
Rolf Kristensen <snakefoot@users.noreply.github.com>
2020-08-10 21:45:06 +0200

    BufferingTargetWrapper - Minor cleanup after changing to AsyncRequestQueue (#4082)

--- src/NLog/Targets/Wrappers/AsyncRequestQueue-T.cs
+++ src/NLog/Targets/Wrappers/AsyncRequestQueue-T.cs
@@ -120,7 +120,7 @@ namespace NLog.Targets.Wrappers
         /// Dequeues a maximum of <c>count</c> items from the queue
         /// and adds returns the list containing them.
         /// </summary>
-        /// <param name="count">Maximum number of items to be dequeued (-1 means everything).</param>
+        /// <param name="count">Maximum number of items to be dequeued</param>
         /// <returns>The array of log events.</returns>
         public override AsyncLogEventInfo[] DequeueBatch(int count)
         {
@@ -128,7 +128,7 @@ namespace NLog.Targets.Wrappers
 
             lock (_logEventInfoQueue)
             {
-                if (count == -1 ||_logEventInfoQueue.Count < count)
+                if (_logEventInfoQueue.Count < count)
                     count = _logEventInfoQueue.Count;
 
                 if (count == 0)
