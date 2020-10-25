6c92960fbe72f5f952c69820587c5230e406377b
njqdev <niklas.jq@outlook.com>
2020-10-25 22:04:41 +0100

    Revert layout change in test

--- tests/NLog.UnitTests/LayoutRenderers/Wrappers/WhenEmptyTests.cs
+++ tests/NLog.UnitTests/LayoutRenderers/Wrappers/WhenEmptyTests.cs
@@ -73,7 +73,7 @@ namespace NLog.UnitTests.LayoutRenderers.Wrappers
         {
             using (new NoThrowNLogExceptions())
             {
-                SimpleLayout l = @"${whenEmpty:whenEmpty=${literal:text=c\:\\logs\\}}api.log";
+                SimpleLayout l = @"${whenEmpty:whenEmpty=${literal:text=c:\logs\}:inner=${environment:LOG_DIR_XXX}}api.log";
                 var le = LogEventInfo.Create(LogLevel.Info, "logger", "message");
                 LogManager.ThrowExceptions = true;
                 Assert.Equal("api.log", l.Render(le));
