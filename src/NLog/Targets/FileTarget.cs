dbcbbcd0c05127df5d2e7f00c57fe802df0e0ebb
TalAloni <tal.aloni.il@gmail.com>
2021-01-19 02:10:46 +0200

    Updated syntax

--- src/NLog/Targets/FileTarget.cs
+++ src/NLog/Targets/FileTarget.cs
@@ -1333,7 +1333,7 @@ namespace NLog.Targets
             else if (EnableArchiveFileCompression)
             {
                 InternalLogger.Info("FileTarget(Name={0}): Archiving {1} to compressed {2}", Name, fileName, archiveFileName);
-#if NET4_5
+#if NET45
                 if (FileCompressor is ZipArchiveFileCompressor zipArchiveFileCompressor)
                 {
                     zipArchiveFileCompressor.IsEntryNameInferredFromArchiveFileName = (ArchiveNumbering != ArchiveNumberingMode.Rolling);
