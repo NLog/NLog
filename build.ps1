e8712e62842e2d74d60fdf37cf74d743750e5ca2
Rolf Kristensen <snakefoot@users.noreply.github.com>
2021-03-24 22:28:52 +0100

    Version 4.7.9 (#4376)
    * Version 4.7.9
    
    * 4.7.9
    
    * Update CHANGELOG.md
    
    Co-authored-by: Julian Verdurmen <304NotModified@users.noreply.github.com>

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ dotnet --version
 # dotnet restore .\src\NLog\
 # dotnet pack .\src\NLog\  --configuration release --include-symbols -o ..\..\artifacts
 
-$versionPrefix = "4.7.8"
+$versionPrefix = "4.7.9"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
