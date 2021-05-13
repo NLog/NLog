eb30edd97300df619cdb92b62c0e95643c72ce9f
Julian Verdurmen <304NotModified@users.noreply.github.com>
2021-05-13 23:00:50 +0200

    Release 4.7.10

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ dotnet --version
 # dotnet restore .\src\NLog\
 # dotnet pack .\src\NLog\  --configuration release --include-symbols -o ..\..\artifacts
 
-$versionPrefix = "4.7.9"
+$versionPrefix = "4.7.10"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
