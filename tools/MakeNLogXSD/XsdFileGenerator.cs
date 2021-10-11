5dfc46ccded4edabe39ae5ccc0ecaa06539af6b4
Rolf Kristensen <sweaty1@hotmail.com>
2021-10-12 00:39:10 +0200

    Replaced DefaultValueAttribute lookup with actual property-value from default-instance

--- tools/MakeNLogXSD/XsdFileGenerator.cs
+++ tools/MakeNLogXSD/XsdFileGenerator.cs
@@ -266,12 +266,12 @@ namespace MakeNLogXSD
         private static XElement GetPropertyElement(XElement propertyElement)
         {
             var result = new XElement(xsd + "element",
-                new XAttribute("name", (string)propertyElement.Attribute("camelName")),
-                new XAttribute("minOccurs", "0"));
+                new XAttribute("name", (string)propertyElement.Attribute("camelName")));
 
             string propertyType = (string)propertyElement.Attribute("type");
             if (propertyType == "Collection")
             {
+                result.Add(new XAttribute("minOccurs", "0"));
                 result.Add(new XAttribute("maxOccurs", "unbounded"));
                 var elementType = propertyElement.Element("elementType");
                 result.Attribute("name").Value = (string)elementType.Attribute("elementTag");
@@ -280,9 +280,12 @@ namespace MakeNLogXSD
             else if (propertyType == "Enum")
             {
                 string enumType = (string)propertyElement.Attribute("enumType");
-
-                result.Add(new XAttribute("maxOccurs", "1"));
                 result.Add(new XAttribute("type", enumType));
+                result.Add(new XAttribute("minOccurs", "0"));
+                result.Add(new XAttribute("maxOccurs", "1"));
+                string defaultValue = (string)propertyElement.Attribute("defaultValue");
+                if (!string.IsNullOrEmpty(defaultValue))
+                    result.Add(new XAttribute("default", defaultValue));
             }
             else
             {
@@ -290,8 +293,17 @@ namespace MakeNLogXSD
                 if (xsdType == null)
                     return null;
 
-                result.Add(new XAttribute("maxOccurs", "1"));
                 result.Add(new XAttribute("type", xsdType));
+                string defaultValue = (string)propertyElement.Attribute("defaultValue");
+                string requiredValue = (string)propertyElement.Attribute("required");
+                if (requiredValue == "1" && defaultValue == null && (xsdType.StartsWith("xs:") || xsdType == "Layout"))
+                    result.Add(new XAttribute("minOccurs", "1"));
+                else
+                    result.Add(new XAttribute("minOccurs", "0"));
+                result.Add(new XAttribute("maxOccurs", "1"));
+                
+                if (!string.IsNullOrEmpty(defaultValue) && xsdType.StartsWith("xs:"))
+                    result.Add(new XAttribute("default", defaultValue));
             }
 
             return result;
