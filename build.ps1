17652ab96278969a212282ecab72a625ea359715
Rolf Kristensen <sweaty1@hotmail.com>
2022-02-21 20:57:48 +0100

    NLog 5.0 Ready for delivery

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@
 dotnet --version
 
 $versionPrefix = "5.0.0"
-$versionSuffix = "rc2"
+$versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
 if (-Not $versionSuffix.Equals(""))
