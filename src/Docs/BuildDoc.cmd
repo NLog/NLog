ace33941c2449159b0563b4f592c9fdb0a699abd
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2022-05-22 15:53:36 +0200

    more verbosity:minimal

--- src/Docs/BuildDoc.cmd
+++ src/Docs/BuildDoc.cmd
@@ -3,8 +3,8 @@
 set BuildVersion=5.0
 
 msbuild /t:restore,build %~dp0\..\NLog.sln /p:Configuration=Release /verbosity:minimal
-msbuild /t:restore,build %~dp0\dll_to_doc /p:RestorePackagesConfig=true /p:Configuration=Release 
-msbuild /t:restore %~dp0NLog.shfbproj /p:Configuration=Release
+msbuild /t:restore,build %~dp0\dll_to_doc /p:RestorePackagesConfig=true /p:Configuration=Release /verbosity:minimal
+msbuild /t:restore %~dp0NLog.shfbproj /p:Configuration=Release /verbosity:minimal
 msbuild %~dp0NLog.shfbproj /p:Configuration=Release /p:Framework=%FRAMEWORK% /p:AssemblyName=NLog /p:BuildVersion=%BuildVersion%
 
 rem copy favicon
