e0d0329eec7a85569ac4934e76cb41d66a720fb5
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2022-05-23 18:39:43 +0200

    DLL to Docs - in csproj SDK (#4935)
    * DLL to Docs - in csproj SDK
    
    * update wildcards and rebuild
    
    Co-authored-by: Julian Verdurmen <304NotModified@users.noreply.github.com>

--- src/Docs/BuildDoc.cmd
+++ src/Docs/BuildDoc.cmd
@@ -2,7 +2,7 @@
 
 set BuildVersion=5.0
 
-msbuild /t:restore,build %~dp0\dll_to_doc /p:RestorePackagesConfig=true /p:Configuration=Release /verbosity:minimal
+msbuild /t:restore,rebuild %~dp0\dll_to_doc /p:Configuration=Release /verbosity:minimal
 msbuild /t:restore %~dp0NLog.shfbproj /p:Configuration=Release /verbosity:minimal
 msbuild %~dp0NLog.shfbproj /p:Configuration=Release /p:Framework=%FRAMEWORK% /p:AssemblyName=NLog /p:BuildVersion=%BuildVersion%
 
