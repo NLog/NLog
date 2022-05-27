a964ef81cb1de86cc806784fd27c032aa15b69d8
Rolf Kristensen <sweaty1@hotmail.com>
2022-05-27 13:36:53 +0200

    Replaced netcoreapp2.1 with netcoreapp3.1 to fix obsolete test warnings

--- tests/NLog.UnitTests/Properties/AssemblyInfo.cs
+++ tests/NLog.UnitTests/Properties/AssemblyInfo.cs
@@ -35,21 +35,6 @@ using System.Reflection;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-
 [assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
 
 [assembly: Xunit.TestFramework("NLogThrowExceptionsDefault", "NLog.UnitTests")]
