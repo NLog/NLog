be1571dfb3ccf5880ab2c3f9f2f268c856a28252
Rolf Kristensen <sweaty1@hotmail.com>
2022-06-10 23:17:52 +0200

    Release NLog 5.0.1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "5.0.0"
+$versionPrefix = "5.0.1"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
