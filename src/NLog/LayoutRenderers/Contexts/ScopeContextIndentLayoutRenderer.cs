7b29efc1c0ef725b052785edfc4bbb54693b1c2f
Rolf Kristensen <sweaty1@hotmail.com>
2022-10-22 14:06:38 +0200

    ScopeIndent LayoutRenderer

---
+++ src/NLog/LayoutRenderers/Contexts/ScopeContextIndentLayoutRenderer.cs
@@ -0,0 +1,67 @@
+﻿// 
+// Copyright (c) 2004-2021 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+namespace NLog.LayoutRenderers
+{
+    using System.Text;
+    using NLog.Config;
+    using NLog.Layouts;
+
+    /// <summary>
+    /// Renders the nested states from <see cref="ScopeContext"/> like a callstack
+    /// </summary>
+    [LayoutRenderer("scopeindent")]
+    public sealed class ScopeContextIndentLayoutRenderer : LayoutRenderer
+    {
+        /// <summary>
+        /// Gets or sets the indent token.
+        /// </summary>
+        /// <docgen category='Layout Options' order='10' />
+        [DefaultParameter]
+        public Layout Indent { get; set; } = "  ";
+
+        /// <inheritdoc/>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            string indent = null;
+
+            var messages = ScopeContext.GetAllNestedStates();
+            for (int i = 0; i < messages.Length; ++i)
+            {
+                indent = indent ?? Indent?.Render(logEvent) ?? string.Empty;
+                builder.Append(indent);
+            }
+        }
+    }
+}
+
