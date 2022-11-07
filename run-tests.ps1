d39fb2a10a3aa46b86c9bbd28910f760bb713ac6
Rolf Kristensen <sweaty1@hotmail.com>
2022-11-07 20:48:32 +0100

    Bump Microsoft.NET.Test.Sdk from 17.3.2 to 17.4.0

--- run-tests.ps1
+++ run-tests.ps1
@@ -23,14 +23,14 @@ if ($isWindows -or $Env:WinDir)
 	dotnet msbuild /t:Build /p:targetFramework=net461 /p:Configuration=Release /p:DebugType=Full /p:TestTargetFramework=net35 ./tests/NLog.UnitTests/
 	if (-Not $LastExitCode -eq 0)
 		{ exit $LastExitCode }
-	dotnet vstest ./tests/NLog.UnitTests/bin/Release/net35/NLog.UnitTests.dll
+	dotnet vstest ./tests/NLog.UnitTests/bin/release/net35/NLog.UnitTests.dll
 	if (-Not $LastExitCode -eq 0)
 		{ exit $LastExitCode }
 
 	dotnet msbuild /t:Build /p:targetFramework=net461 /p:Configuration=Release /p:DebugType=Full /p:TestTargetFramework=net45 ./tests/NLog.UnitTests/
 	if (-Not $LastExitCode -eq 0)
 		{ exit $LastExitCode }
-	dotnet vstest ./tests/NLog.UnitTests/bin/Release/net45/NLog.UnitTests.dll
+	dotnet vstest ./tests/NLog.UnitTests/bin/release/net45/NLog.UnitTests.dll
 	if (-Not $LastExitCode -eq 0)
 		{ exit $LastExitCode }
 
@@ -43,15 +43,19 @@ if ($isWindows -or $Env:WinDir)
 }
 else
 {
-	# Need help from MONO to run normal .NetFramework tests
-	dotnet msbuild /t:Restore,Rebuild /p:targetframework=net461 /p:Configuration=Release /p:DebugType=Full /p:monobuild=1 tests/NLog.UnitTests
-    if (-Not $LastExitCode -eq 0)
-	    { exit $LastExitCode }
-	dotnet vstest ./tests/NLog.UnitTests/bin/Release/net461/NLog.UnitTests.dll
-    if (-Not $LastExitCode -eq 0)
-	    { exit $LastExitCode }
-
 	dotnet test ./tests/NLog.Database.Tests/ --framework netcoreapp3.1 --configuration release
 	if (-Not $LastExitCode -eq 0)
 		{ exit $LastExitCode }
+
+	# Need help from MONO to run normal .NetFramework tests
+	dotnet msbuild /t:restore ./tests/NLog.UnitTests/ /p:RestoreForce=true /p:monobuild=1
+	if (-Not $LastExitCode -eq 0)
+		{ exit $LastExitCode }
+	dotnet build ./tests/NLog.UnitTests/ --framework net461 --configuration release --no-restore --no-incremental /p:monobuild=1
+    if (-Not $LastExitCode -eq 0)
+	    { exit $LastExitCode }
+
+	dotnet vstest ./tests/NLog.UnitTests/bin/release/net461/NLog.UnitTests.dll
+    if (-Not $LastExitCode -eq 0)
+	    { exit $LastExitCode }
 }
