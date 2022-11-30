643e179f7f78152ffd92426083e199decf992a0b
Rolf Kristensen <sweaty1@hotmail.com>
2022-11-30 18:34:29 +0100

    No more twitter

--- tests/NLog.UnitTests/LayoutRenderers/ProcessNameLayoutRendererTests.cs
+++ tests/NLog.UnitTests/LayoutRenderers/ProcessNameLayoutRendererTests.cs
@@ -56,7 +56,7 @@ namespace NLog.UnitTests.LayoutRenderers
             var lower = actual.ToLower();
 
             //lowercase
-            var allowedProcessNames = new List<string> {"vstest.executionengine", "xunit", "mono-sgen", "dotnet", "testhost.x86", "testhost.x64", "testhost.net452" };
+            var allowedProcessNames = new List<string> {"vstest.executionengine", "xunit", "mono-sgen", "dotnet", "testhost", "testhost.x86", "testhost.x64", "testhost.net452" };
 
             Assert.True(allowedProcessNames.Any(p => lower.Contains(p)),
                 $"validating processname failed. Please add (if correct) '{actual}' to 'allowedProcessNames'");
