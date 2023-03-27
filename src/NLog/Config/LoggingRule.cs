b4b772fabe87e507e750ece0a09c354132ae7ff8
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-03-27 21:40:35 +0200

    Update LoggingRule.cs

--- src/NLog/Config/LoggingRule.cs
+++ src/NLog/Config/LoggingRule.cs
@@ -165,12 +165,10 @@ namespace NLog.Config
         public string LoggerNamePattern
         {
             get => _loggerNameMatcher.Pattern;
-
-            set
-            {
-                _loggerNameMatcher = LoggerNameMatcher.Create(value);
-            }
+            set => _loggerNameMatcher = LoggerNameMatcher.Create(value);
         }
+
+        internal bool[] LogLevels => _logLevelFilter.LogLevels;
 
         /// <summary>
         /// Gets the collection of log levels enabled by this rule.
@@ -230,24 +228,14 @@ namespace NLog.Config
             _logLevelFilter = _logLevelFilter.GetSimpleFilterForUpdate().SetLoggingLevels(minLevel, maxLevel, true);
         }
 
-        internal void EnableLoggingForLevelLayout(NLog.Layouts.SimpleLayout simpleLayout)
+        internal void EnableLoggingForLevelLayout(NLog.Layouts.SimpleLayout simpleLayout, NLog.Layouts.SimpleLayout finalMinLevel)
         {
-            _logLevelFilter = new DynamicLogLevelFilter(this, simpleLayout);
+            _logLevelFilter = new DynamicLogLevelFilter(this, simpleLayout, finalMinLevel);
         }
 
-        internal void EnableLoggingForLevelsLayout(Layouts.SimpleLayout minLevel, Layouts.SimpleLayout maxLevel)
+        internal void EnableLoggingForLevelsLayout(Layouts.SimpleLayout minLevel, Layouts.SimpleLayout maxLevel, NLog.Layouts.SimpleLayout finalMinLevel)
         {
-            _logLevelFilter = new DynamicRangeLevelFilter(this, minLevel, maxLevel);
-        }
-
-        internal void EnableLoggingForFinalMinLevel(LogLevel finalMinLevel)
-        {
-            _logLevelFilter = new FinalMinLogLevelFilter(finalMinLevel);
-        }
-
-        internal void EnableLoggingForFinalMinLevelLayout(Layouts.SimpleLayout finalMinLevel)
-        {
-            _logLevelFilter = new FinalMinLogLevelLayoutFilter(this, finalMinLevel);
+            _logLevelFilter = new DynamicRangeLevelFilter(this, minLevel, maxLevel, finalMinLevel);
         }
 
         /// <summary>
