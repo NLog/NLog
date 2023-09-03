eb775a34b5999f53ef2159d7e826c75b03cda6f3
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-09-03 11:16:37 +0200

    JsonLayout - Support minimal thread context capture from inner layouts (#5340)

--- src/NLog/Layouts/Layout.cs
+++ src/NLog/Layouts/Layout.cs
@@ -406,7 +406,7 @@ namespace NLog.Layouts
                 }
             }
 
-            if (layoutCount <= 1 || layoutCount == precalculateSimpleLayoutCount || precalculateSimpleLayoutCount > 5 || (precalculateLayoutCount - precalculateSimpleLayoutCount) > 2)
+            if (layoutCount <= 1 || precalculateLayoutCount > 4 || (precalculateLayoutCount - precalculateSimpleLayoutCount) > 2 || (layoutCount - precalculateSimpleLayoutCount) <= 1)
             {
                 return null;
             }
