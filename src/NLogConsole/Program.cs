9d404aec3873c89b774d057c4bcb369547d1088a
Francis Chung <francis.c.chung@gmail.com>
2024-03-06 11:10:19 +1100

    Test Console Project for Prototyping

---
+++ src/NLogConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using NLog.Fluent;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, NLogConsole!");
+
+Logger log = LogManager.GetCurrentClassLogger();
+log.Trace("This is a Trace message FC");
+//log.Debug("This is a debug message");
+//log.Error(new Exception(), "This is an error message");
+//log.Fatal("This is a fatal message");
+
+//log.Info()
+//    .Message("This is a Fluent Info Message")
+//    .Property("TestProp", "SomeProp")
+//    .Write();
+
+
+
+Console.WriteLine("The End!");
+Console.ReadLine();
