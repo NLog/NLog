9ea4d7d655a74dc838a86682c8b831168dc28597
Rolf Kristensen <sweaty1@hotmail.com>
2024-03-30 11:45:25 +0100

    Version 5.3

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "5.2.8"
+$versionPrefix = "5.3.0"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
