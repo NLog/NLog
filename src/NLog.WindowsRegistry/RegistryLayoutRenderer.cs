0ba276621183df2eaab033df96262258a73f6bfa
James Thompson <thompson.tomo@outlook.com>
2024-04-07 19:34:30 +1000

    Whitespace restore

--- src/NLog.WindowsRegistry/RegistryLayoutRenderer.cs
+++ src/NLog.WindowsRegistry/RegistryLayoutRenderer.cs
@@ -1,35 +1,35 @@
-//
+// 
 // Copyright (c) 2004-2021 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
-//
+// 
 // All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
 // are met:
-//
-// * Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the following disclaimer.
-//
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
 // * Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-//
-// * Neither the name of Jaroslaw Kowalski nor the names of its
+//   and/or other materials provided with the distribution. 
+// 
+// * Neither the name of Jaroslaw Kowalski nor the names of its 
 //   contributors may be used to endorse or promote products derived from this
-//   software without specific prior written permission.
-//
+//   software without specific prior written permission. 
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
 // CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 // THE POSSIBILITY OF SUCH DAMAGE.
-//
+// 
 
 namespace NLog.LayoutRenderers
 {
@@ -61,9 +61,9 @@ namespace NLog.LayoutRenderers
 
         /// <summary>
         /// Require escaping backward slashes in <see cref="DefaultValue"/>. Need to be backwards-compatible.
-        ///
+        /// 
         /// When true:
-        ///
+        /// 
         /// `\` in value should be configured as `\\`
         /// `\\` in value should be configured as `\\\\`.
         /// </summary>
@@ -73,8 +73,8 @@ namespace NLog.LayoutRenderers
 
 #if !NET35
         /// <summary>
-        /// Gets or sets the registry view (see: https://msdn.microsoft.com/de-de/library/microsoft.win32.registryview.aspx).
-        /// Allowed values: Registry32, Registry64, Default
+        /// Gets or sets the registry view (see: https://msdn.microsoft.com/de-de/library/microsoft.win32.registryview.aspx). 
+        /// Allowed values: Registry32, Registry64, Default 
         /// </summary>
         /// <docgen category='Registry Options' order='10' />
         public RegistryView View { get; set; } = RegistryView.Default;
@@ -220,7 +220,7 @@ namespace NLog.LayoutRenderers
             {"HKEY_CLASSES_ROOT", RegistryHive.ClassesRoot},
             {"HKEY_USERS", RegistryHive.Users},
             {"HKEY_CURRENT_CONFIG", RegistryHive.CurrentConfig},
-#if NET35 || NET46
+#if NETFRAMEWORK
             {"HKEY_DYN_DATA", RegistryHive.DynData},
 #endif
             {"HKEY_PERFORMANCE_DATA", RegistryHive.PerformanceData},
