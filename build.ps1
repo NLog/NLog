1873145cbc26e0312d9481234d69ce7dc68aac4e
Rolf Kristensen <snakefoot@users.noreply.github.com>
2025-05-29 10:12:32 +0200

    Version 5.5 (#5848)

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "5.4.0"
+$versionPrefix = "5.5.0"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
