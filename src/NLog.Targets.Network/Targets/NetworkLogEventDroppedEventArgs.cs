f1dfab74a60a8feab13154fd8f127b382169516c
Rolf Kristensen <sweaty1@hotmail.com>
2025-07-12 12:25:37 +0200

    SplunkTarget - NetworkTarget with SplunkLayout

--- src/NLog.Targets.Network/Targets/NetworkLogEventDroppedEventArgs.cs
+++ src/NLog.Targets.Network/Targets/NetworkLogEventDroppedEventArgs.cs
@@ -31,10 +31,10 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-using System;
-
 namespace NLog.Targets
 {
+    using System;
+
     /// <summary>
     /// Arguments for <see cref="NetworkTarget.LogEventDropped"/> events.
     /// </summary>
