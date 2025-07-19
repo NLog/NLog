9a663870830727a3903004de745a68d901227909
Rolf Kristensen <sweaty1@hotmail.com>
2025-07-19 17:33:17 +0200

    NLog v6.0.2

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "6.0.1"
+$versionPrefix = "6.0.2"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
