e276e1969a52b8ecc5cc84c1670a516efea0cb19
Rolf Kristensen <snakefoot@users.noreply.github.com>
2025-08-10 20:18:57 +0200

    Version 6.0.3 (#5959)

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "6.0.2"
+$versionPrefix = "6.0.3"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
