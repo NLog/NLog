5c44cfc63e3be2e2a1d40e0de27e5d088ee457b9
Rolf Kristensen <sweaty1@hotmail.com>
2026-05-16 17:45:02 +0200

    Enum parse from string maps empty string to default value (no flags)

--- src/NLog/Common/ConversionHelpers.cs
+++ src/NLog/Common/ConversionHelpers.cs
@@ -61,16 +61,20 @@ namespace NLog.Common
             return true;
         }
 
-        /// <summary>
-        /// Converts input string value into <see cref="System.Enum"/>. Parsing is case-insensitive.
-        /// </summary>
-        /// <param name="inputValue">Input value</param>
-        /// <param name="enumType">The type of the enum</param>
-        /// <param name="resultValue">Output value. Null if parse failed</param>
+        /// <summary>Try parse string as enum value (case-insensitive). Returns default enum value for null/whitespace input.</summary>
+        /// <param name="inputValue">String to parse.</param>
+        /// <param name="enumType">Target enum type.</param>
+        /// <param name="resultValue">Parsed enum value, or <see langword="null"/> on failure.</param>
+        /// <returns><see langword="true"/> if parsed successfully or input was null/whitespace; otherwise <see langword="false"/>.</returns>
         internal static bool TryParseEnum(string inputValue, Type enumType, out object? resultValue)
         {
             if (StringHelpers.IsNullOrWhiteSpace(inputValue))
             {
+                if (enumType.IsEnum)
+                {
+                    resultValue = Enum.ToObject(enumType, 0);
+                    return true;
+                }
                 resultValue = null;
                 return false;
             }
@@ -94,21 +98,19 @@ namespace NLog.Common
 #endif
         }
 
-        /// <summary>
-        /// Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object. A parameter specifies whether the operation is case-sensitive. The return value indicates whether the conversion succeeded.
-        /// </summary>
-        /// <typeparam name="TEnum">The enumeration type to which to convert value.</typeparam>
-        /// <param name="inputValue">The string representation of the enumeration name or underlying value to convert.</param>
+        /// <summary>Try parse string as enum value. Returns default enum value for null/whitespace input.</summary>
+        /// <typeparam name="TEnum">Target enum type.</typeparam>
+        /// <param name="inputValue">String to parse.</param>
         /// <param name="ignoreCase"><see langword="true"/> to ignore case; <see langword="false"/> to consider case.</param>
-        /// <param name="resultValue">When this method returns, result contains an object of type TEnum whose value is represented by value if the parse operation succeeds. If the parse operation fails, result contains the default value of the underlying type of TEnum. Note that this value need not be a member of the TEnum enumeration. This parameter is passed uninitialized.</param>
-        /// <returns><see langword="true"/> if the value parameter was converted successfully; otherwise, <see langword="false"/>.</returns>
-        /// <remarks>Wrapper because Enum.TryParse is not present in .net 3.5</remarks>
+        /// <param name="resultValue">Parsed enum value, or default on failure.</param>
+        /// <returns><see langword="true"/> if parsed successfully or input was null/whitespace; otherwise <see langword="false"/>.</returns>
+        /// <remarks>Wrapper because Enum.TryParse is not present in .NET 3.5</remarks>
         internal static bool TryParseEnum<TEnum>(string inputValue, bool ignoreCase, out TEnum resultValue) where TEnum : struct
         {
             if (StringHelpers.IsNullOrWhiteSpace(inputValue))
             {
                 resultValue = default(TEnum);
-                return false;
+                return true;
             }
 
 #if !NET35
--- tests/NLog.UnitTests/Internal/ConversionHelpersTests.cs
+++ tests/NLog.UnitTests/Internal/ConversionHelpersTests.cs
@@ -39,10 +39,19 @@ namespace NLog.UnitTests.Internal
 
     public class ConversionHelpersTests : NLogTestBase
     {
-        enum TestEnum
+        enum TestEnumType
         {
+            None,
             Foo,
-            bar,
+            Bar,
+        }
+
+        [Flags]
+        enum TestEnumTypes
+        {
+            None = 0,
+            Foo = 1,
+            Bar = 2,
         }
 
         #region tryparse - ignorecase parameter: false
@@ -50,68 +59,68 @@ namespace NLog.UnitTests.Internal
         [Fact]
         public void EnumParse1_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("Foo", false, TestEnum.Foo, true);
+            TestEnumParseCaseIgnoreCaseParam("Foo", false, TestEnumType.Foo, true);
         }
 
         [Fact]
         public void EnumParse2_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("foo", false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("foo", false, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseDefault_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("BAR", false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("BAR", false, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseDefault2_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("x", false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("x", false, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseBar_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("bar", false, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam("bar", false, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseBar2_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam(" bar ", false, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam(" bar ", false, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseBar3_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam(" \r\nbar ", false, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam(" \r\nbar ", false, TestEnumType.None, false);
         }
 
 
         [Fact]
         public void EnumParse_null_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam(null, false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam(null, false, TestEnumType.None, true);
         }
 
         [Fact]
         public void EnumParse_emptystring_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam(string.Empty, false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam(string.Empty, false, TestEnumType.None, true);
         }
 
         [Fact]
         public void EnumParse_whitespace_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("   ", false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("   ", false, TestEnumType.None, true);
         }
 
         [Fact]
         public void EnumParse_wrongInput_ignoreCaseFalse()
         {
-            TestEnumParseCaseIgnoreCaseParam("not enum", false, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("not enum", false, TestEnumType.None, false);
         }
 
         #endregion
@@ -121,61 +130,61 @@ namespace NLog.UnitTests.Internal
         [Fact]
         public void EnumParse1_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("Foo", true, TestEnum.Foo, true);
+            TestEnumParseCaseIgnoreCaseParam("Foo", true, TestEnumType.Foo, true);
         }
 
         [Fact]
         public void EnumParse2_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("foo", true, TestEnum.Foo, true);
+            TestEnumParseCaseIgnoreCaseParam("foo", true, TestEnumType.Foo, true);
         }
 
         [Fact]
         public void EnumParseDefault_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("BAR", true, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam("BAR", true, TestEnumType.Bar, true);
         }
 
         [Fact]
         public void EnumParseDefault2_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("x", true, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("x", true, TestEnumType.None, false);
         }
 
         [Fact]
         public void EnumParseBar_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("bar", true, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam("bar", true, TestEnumType.Bar, true);
         }
 
         [Fact]
         public void EnumParseBar2_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam(" bar ", true, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam(" bar ", true, TestEnumType.Bar, true);
         }
 
         [Fact]
         public void EnumParseBar3_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam(" \r\nbar ", true, TestEnum.bar, true);
+            TestEnumParseCaseIgnoreCaseParam(" \r\nbar ", true, TestEnumType.Bar, true);
         }
 
         [Fact]
         public void EnumParse_null_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam(null, true, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam(null, true, TestEnumType.None, true);
         }
 
         [Fact]
         public void EnumParse_emptystring_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam(string.Empty, true, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam(string.Empty, true, TestEnumType.None, true);
         }
 
         [Fact]
         public void EnumParse_whitespace_ignoreCaseTrue()
         {
-            TestEnumParseCaseIgnoreCaseParam("   ", true, TestEnum.Foo, false);
+            TestEnumParseCaseIgnoreCaseParam("   ", true, TestEnumType.None, true);
         }
 
         [Fact]
@@ -187,14 +196,80 @@ namespace NLog.UnitTests.Internal
             Assert.Throws<ArgumentException>(() => ConversionHelpers.TryParseEnum("not enum", typeof(double), out _));
         }
 
+        [Fact]
+        public void EnumParse_nonGeneric_nonEnumType_null_returnsFalse()
+        {
+            // null/whitespace input with a non-enum type must return false + null (no Enum.ToObject call)
+            var returnResult = ConversionHelpers.TryParseEnum(null, typeof(double), out var result);
+            Assert.False(returnResult);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_null_returnsNoneAndTrue()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum(null, true, out TestEnumTypes result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.None, result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_emptystring_returnsNoneAndTrue()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum(string.Empty, true, out TestEnumTypes result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.None, result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_whitespace_returnsNoneAndTrue()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum("   ", true, out TestEnumTypes result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.None, result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_nonGeneric_null_returnsNoneAndTrue()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum(null, typeof(TestEnumTypes), out var result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.None, result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_nonGeneric_whitespace_returnsNoneAndTrue()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum("   ", typeof(TestEnumTypes), out var result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.None, result);
+        }
+
+        [Fact]
+        public void EnumParseFlags_validValue_parsesCorrectly()
+        {
+            var returnResult = ConversionHelpers.TryParseEnum("Foo, Bar", true, out TestEnumTypes result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumTypes.Foo | TestEnumTypes.Bar, result);
+        }
+
+        [Fact]
+        public void EnumParse_nonGeneric_isAlwaysCaseInsensitive()
+        {
+            // The non-generic (Type) overload always parses case-insensitively
+            var returnResult = ConversionHelpers.TryParseEnum("foo", typeof(TestEnumType), out var result);
+            Assert.True(returnResult);
+            Assert.Equal(TestEnumType.Foo, result);
+        }
+
         #endregion
 
         #region helpers
 
-        private static void TestEnumParseCaseIgnoreCaseParam(string value, bool ignoreCase, TestEnum expected, bool expectedReturn)
+        private static void TestEnumParseCaseIgnoreCaseParam(string value, bool ignoreCase, TestEnumType expected, bool expectedReturn)
         {
             {
-                var returnResult = ConversionHelpers.TryParseEnum(value, ignoreCase, out TestEnum result);
+                var returnResult = ConversionHelpers.TryParseEnum(value, ignoreCase, out TestEnumType result);
 
                 Assert.Equal(expected, result);
                 Assert.Equal(expectedReturn, returnResult);
@@ -205,13 +280,13 @@ namespace NLog.UnitTests.Internal
             {
                 {
 #pragma warning disable CS0618 // Type or member is obsolete
-                    var returnResult = ConversionHelpers.TryParseEnum<TestEnum>(value, out var result);
+                    var returnResult = ConversionHelpers.TryParseEnum<TestEnumType>(value, out var result);
                     Assert.Equal(expected, result);
                     Assert.Equal(expectedReturn, returnResult);
 #pragma warning restore CS0618 // Type or member is obsolete
                 }
                 {
-                    var returnResult = ConversionHelpers.TryParseEnum(value, typeof(TestEnum), out var result);
+                    var returnResult = ConversionHelpers.TryParseEnum(value, typeof(TestEnumType), out var result);
                     Assert.Equal(expectedReturn, returnResult);
                     if (expectedReturn)
                     {
