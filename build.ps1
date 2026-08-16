bc6d0f685191b1bc019a5d4f940b8f2accfe5f94
Rolf Kristensen <sweaty1@hotmail.com>
2026-08-16 21:16:53 +0200

    Version 6.2

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "6.1.4"
+$versionPrefix = "6.2.0"
 $versionSuffix = ""
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 $versionProduct = $versionPrefix;
